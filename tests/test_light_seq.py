5ca3452bd4de6720cf1899a4897f76d8a4c89db3
anujp <anuj.pahwa@gmail.com>
2010-07-04 13:56:55 +1000

    Modified LightSeq class to handle fasta and fastq conversions. Updated test_light_seq to test this functionality.

--- tests/test_light_seq.py
+++ tests/test_light_seq.py
@@ -7,6 +7,8 @@ from light_seq import LightSeq
 test_seq_name = 'GAPC_0015:6:1:1259:10413#0/1'
 test_seq = 'AACACCAAACTTCTCCACCACGTGAGCTACAAAAG'
 test_seq_qual = r'````Y^T]`]c^cabcacc`^Lb^ccYT\T\Y\WF'
+test_seq_fasta = '>GAPC_0015:6:1:1259:10413#0/1\nAACACCAAACTTCTCCACCACGTGAGCTACAAAAG'
+test_seq_fastq = '>GAPC_0015:6:1:1259:10413#0/1\nAACACCAAACTTCTCCACCACGTGAGCTACAAAAG\n>GAPC_0015:6:1:1259:10413#0/1\n````Y^T]`]c^cabcacc`^Lb^ccYT\T\Y\WF'
 
 class LightSeqTest(TestCase):
     def test_load(self):
@@ -26,7 +28,7 @@ class LightSeqTest(TestCase):
         # Illumina schema allows for quality scores between 2 and 40 where 2 is
         # is considered a Read Segment Quality Control Indicator (towards the end
         # of the read
-        qualScores = sequence.get_numeric_quality(scheme='illumina')
+        qualScores = sequence.getNumericQuality(scheme='illumina')
         for score in qualScores:
             self.assertTrue(2 <= score <= 40)
 
@@ -39,6 +41,23 @@ class LightSeqTest(TestCase):
         self.assertEqual(new_sequence.seq, test_seq[:-10])
         self.assertEqual(new_sequence.Quality, test_seq_qual[:-10])
 
+    def test_toFasta(self):
+        """ test fasta conversion"""
+
+        sequence = LightSeq(Seq=test_seq, Name=test_seq_name, Quality=test_seq_qual)
+        self.assertEqual(sequence.toFasta(), test_seq_fasta)
+
+    def test_toFastq(self):
+        """ test fastq conversion"""
+
+        # Quality score specified at creation of object
+        sequence = LightSeq(Seq=test_seq, Name=test_seq_name, Quality=test_seq_qual)
+        self.assertEqual(sequence.toFastq(), test_seq_fastq)
+
+
+        # Quality score specified when creating fastq string
+        sequence = LightSeq(Seq=test_seq, Name=test_seq_name)
+        self.assertEqual(sequence.toFastq(test_seq_qual), test_seq_fastq)
 
 if __name__ == "__main__":
     main()

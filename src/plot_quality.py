51ebde1f4e8f4ba88279511f9167fa9e1ec4e9bb
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-07-15 11:37:20 +1000

    mods to plot light seq
    [CHANGED] the confidence intervals need to lie within the limits
        of the statistics. For Illumina data that's 2-40, inclusive.

--- src/plot_quality.py
+++ src/plot_quality.py
@@ -6,33 +6,36 @@ from inline_stats import RunningStats
 from parse_fastq import FastqParser
 
 def plot_quality(input_file, output_file=None):
-
+    
     if output_file is None:
         output_file = 'quality.png'
-
+    
     stats = RunningStats()
     for seq in FastqParser(input_file, numeric_qual=False, make_seq=LightSeq):
         stats(seq.getNumericQuality())
-
+    
     # obtain the mean and standard deviation accross all bases
     mean = stats.Mean
     sd = stats.SD
-
+    
+    upper = [min(mean[i]+2*sd[i], 40) for i in range(mean.shape[0])]
+    lower = [max(mean[i]-2*sd[i], 2) for i in range(mean.shape[0])]
+    
     # plot the results
     x = np.arange(stats.length) # length of the longest sequence
-    plt.plot(x, mean, x, mean+2*sd, 'r--', x, mean-2*sd, 'r--')
+    plt.plot(x, mean, x, upper, 'r--', x, lower, 'r--')
     plt.xlabel('Base Number')
     plt.ylabel('Quality Scores')
     plt.title('Quality Scores Across All Bases')
     plt.legend(('Mean', 'SD'), loc='upper right')
-    plt.fill_between(x,mean+2*sd, mean-2*sd, alpha=0.1, color='c')
+    plt.fill_between(x, upper, lower, alpha=0.1, color='c')
     plt.setp(plt.gca(), 'ylim', [0,50])
     plt.savefig(output_file)
 
 if __name__ == "__main__":
     from cogent.util.misc import parse_command_line_parameters
     from optparse import make_option
-
+    
     script_info = {}
     descr = "Get Quality Information from a fastq file and plot the quality "\
             "scores across all bases."
@@ -43,7 +46,7 @@ if __name__ == "__main__":
     script_info['script_usage'].append(
         ("Example 1","""General Usage:""",
         """python plot_quality.py -i <seqs.fastq> -o <qual_plot.png>"""))
-
+    
     script_info['help_on_no_arguments'] = True
     script_info['required_options'] = [
         make_option('-i','--input_file', help='The input fastq sequence file')]

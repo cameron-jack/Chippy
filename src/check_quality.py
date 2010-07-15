4f9ca22bc826335fd4d9e73895b8e464a95a02ce
anujp <anuj.pahwa@gmail.com>
2010-07-15 19:16:19 +1000

    Cleaned up whitespace

---
+++ src/check_quality.py
@@ -0,0 +1,30 @@
+from parse_psl import MinimalPslParser
+from cogent.parse.fasta import MinimalFastaParser
+
+def eval_psl(psl_name):
+    to_trim = set()
+    psl_parser = MinimalPslParser(psl_name)
+    psl_parser.next()
+    psl_parser.next()
+    num = 0
+    for record in psl_parser:
+        name = record[9]
+        num += 1
+        to_trim.update([name])
+    
+    print 'Number seqs with hits to primer', num
+    print 'Number unique seqs with hits to primer', len(to_trim)
+
+def eval_fasta(fasta_name):
+    num = 0
+    for line in open(fasta_name):
+        if line[0] == '>':
+            num += 1
+    print 'Total number seqs', num
+
+psl_name = '../data/result_s_6.psl'
+# eval_psl(psl_name)
+
+
+fasta_name = '../data/s_6.fasta'
+eval_fasta(fasta_name)

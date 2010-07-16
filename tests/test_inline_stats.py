c5f0a8113c7661feef76820d7fb53336575c2b5e
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-07-16 12:22:06 +1000

    drop stats pickle file from repo
    [CHANGED] now written/read then deleted during tests

--- tests/test_inline_stats.py
+++ tests/test_inline_stats.py
@@ -3,6 +3,8 @@ import sys
 sys.path.append('../src')
 
 from cogent.util.unit_test import TestCase, main
+from cogent.util.misc import remove_files
+
 from light_seq import LightSeq
 from inline_stats import RunningStats
 from parse_fastq import FastqParser
@@ -12,9 +14,10 @@ from parse_fastq import FastqParser
 class RunningStatsTest(TestCase):
 
 
-    def test_load(self):
-        """Making sure that the loading of the Running Stats data works"""
-        seqs = [a for a in FastqParser('data/test.txt', numeric_qual=False, make_seq=LightSeq)]
+    def test_save_load(self):
+        """Making sure that saving / loading Running Stats data works"""
+        seqs = [a for a in FastqParser('data/test.txt', numeric_qual=False,
+                                        make_seq=LightSeq)]
         stats = RunningStats()
         maxSeqLength = 0
         for seq in seqs:
@@ -23,32 +26,35 @@ class RunningStatsTest(TestCase):
             if seqLength > maxSeqLength:
                 maxSeqLength = seqLength
         
-        # Also try initialising the object from stored file
-        stats.storeStats('data/test_stats.pkl')
-        loaded_stats = RunningStats(in_file='data/test_stats.pkl')
+        # Also try saving / initialising the object from stored file
+        pickle_file = 'data/test_stats.pkl'
+        stats.storeStats(pickle_file)
+        loaded_stats = RunningStats(in_file=pickle_file)
         self.assertFloatEqual(stats.Mean, loaded_stats.Mean)
+        remove_files([pickle_file])
     
     def test_stats(self):
         """Test whether the mean and the std. deviation computed by RunningStats
         approximates real mean and std. deviation. Mean should be exact, however
         standard deviation is an approximation"""
         
-        seqs = [a for a in FastqParser('data/test.txt', numeric_qual=False, make_seq=LightSeq)]
+        seqs = [a for a in FastqParser('data/test.txt', numeric_qual=False,
+                make_seq=LightSeq)]
         stats = RunningStats()
-
+        
         qualList = []
         for seq in seqs:
             stats(seq.getNumericQuality())
             qualList.append(seq.getNumericQuality())
-
+        
         # compute actual values
         qualArray = numpy.array(qualList)
         mean = qualArray.mean(axis=0)
         sd = numpy.around(qualArray.std(axis=0), 2)
-
+        
         self.assertEqual(stats.Mean, mean)
         self.assertFloatEqualRel(stats.SD, sd, eps=0.05)
-
+    
 
 if __name__ == "__main__":
     main()

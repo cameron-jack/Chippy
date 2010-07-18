5143457fa88bc781334932959326e971012e1c99
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-07-18 23:05:08 +1000

    major changes to computing stats
    [NEW] quickest approach is to just use a 2D dict, keyed by quality char
    [CHANGED] RunningStats no longer being used inline, so this module needs
        a name change. Currently invoked at end only.
    [CHANGED] a sample frequency option is provided to improve performance.
        Default is exhaustive (every quality string is used).
    
    Note: currently, computing quantiles is the slowest part. Inefficiency derives
        fact that a large vector is being created, sorted, then quantiles computed.
        It seems most of this is unecessary.

--- src/plot_quality.py
+++ src/plot_quality.py
@@ -3,33 +3,47 @@ import matplotlib.pyplot as plt
 
 from light_seq import LightSeq
 from inline_stats import RunningStats
-from parse_fastq import FastqParser
+from parse_fastq import FastqParser, MinimalFastqParser
 
-def plot_quality(input_file, upper_quantile, lower_quantile, output_file=None):
+def make_display(stats, output_file, upper_quantile, lower_quantile):
+    print 'Computing stats'
     
-    if output_file is None:
-        output_file = 'quality.png'
-    
-    stats = RunningStats()
-    for seq in FastqParser(input_file, numeric_qual=False, make_seq=LightSeq):
-        stats(seq.getNumericQuality())
-    
-    # obtain the mean and standard deviation accross all bases
-    median = stats.quantile(0.5)
-    
-    upper = stats.quantile(upper_quantile)#[min(mean[i]+2*sd[i], 40) for i in range(mean.shape[0])]
-    lower = stats.quantile(lower_quantile)#[max(mean[i]-2*sd[i], 2) for i in range(mean.shape[0])]
-    
+    lower, median, upper = stats.quantiles([0.05, 0.5, 0.95])
     # plot the results
     x = np.arange(stats.length) # length of the longest sequence
     plt.plot(x, median, x, upper, 'r--', x, lower, 'r--')
     plt.xlabel('Base Number')
     plt.ylabel('Quality Scores')
     plt.title('Quality Scores Across All Bases')
-    plt.legend(('Median', 'Quantiles [%.2f - %.2f]' % (lower_quantile, upper_quantile)), loc='upper right')
+    plt.legend(('Median', 'Quantiles [%.2f - %.2f]' % (lower_quantile,
+                                                       upper_quantile)),
+                                                       loc='upper right')
     plt.fill_between(x, upper, lower, alpha=0.1, color='c')
     plt.setp(plt.gca(), 'ylim', [0,50])
     plt.savefig(output_file)
+
+def plot_quality(input_file, upper_quantile, lower_quantile, sample_freq,
+        output_file=None, limit=None):
+    
+    if output_file is None:
+        output_file = 'quality.png'
+    
+    data = dict([(chr(i+66), [0]*75) for i in range(38)])
+    num = 0
+    for label, seq, qual in MinimalFastqParser(input_file):
+        if num % 100000 == 0:
+            print 'Did seq %d' % num
+        if limit is not None and num > limit:
+            break
+        
+        num += 1
+        if num % sample_freq:
+            for i in range(75):
+                data[qual[i]][i] += 1
+            
+    
+    stats = RunningStats(data)
+    make_display(stats, output_file, upper_quantile, lower_quantile)
 
 if __name__ == "__main__":
     from cogent.util.misc import parse_command_line_parameters
@@ -56,9 +70,14 @@ if __name__ == "__main__":
         make_option('-u', '--upper_quantile', type='float', default=0.95,
             help='upper quantile [default: %default]'),
         make_option('-l', '--lower_quantile', type='float', default=0.05,
-            help='lower quantile [default: %default]')]
+            help='lower quantile [default: %default]'),
+        make_option('-s', '--sample_freq', type='int', default=1,
+            help='sample frequency [default: %default]'),
+            ]
     parser, opts, args = parse_command_line_parameters(**script_info)
     plot_quality(opts.input_file,
         opts.upper_quantile,
         opts.lower_quantile,
-        opts.output_file)
+        opts.output_file,
+        opts.sample_freq)
+    

486bc2701b8db89ceecbb69988ccb4d6d837fd44
anujp <anuj.pahwa@gmail.com>
2010-07-22 19:22:35 +1000

    get_pristine_seqs now outputs two fastq files - one with contaminated and the other with pristine sequences. notes.txt has been updated

--- src/get_pristine_seqs.py
+++ src/get_pristine_seqs.py
@@ -1,6 +1,6 @@
-from cogent.parse.fasta import MinimalFastaParser
-
 from parse_psl import MinimalPslParser
+from parse_fastq import MinimalFastqParser
+from light_seq import LightSeq
 
 def get_corrupt_seq_names(psl_name, test_run):
     psl_parser = MinimalPslParser(psl_name)
@@ -20,31 +20,39 @@ def get_corrupt_seq_names(psl_name, test_run):
 
 to_fasta = lambda name, seq: '\n'.join(['>%s' % name, seq])
 
-def write_pristine(fasta_name, output_file, not_pristine, test_run):
+def write_pristine(fastq_name, outfile_root, not_pristine, test_run):
     num = 0
     if not test_run:
-        outfile = open(output_file, 'w')
+        outfile_pristine = open(outfile_root + '_pristine.fastq', 'w')
+        outfile_contaminated = open(outfile_root + '_contaminated.fastq', 'w')
 
-    for name, seq in MinimalFastaParser(open(fasta_name)):
+
+    for name, seq, qual in MinimalFastqParser(open(fastq_name)):
         num += 1
-        if name in not_pristine:
-            continue
+        seq_object = LightSeq(seq, name, qual)
+        fastq_formatted = seq_object.toFastq()
 
-        fasta_formatted = to_fasta(name, seq)
         if test_run:
-            print fasta_formatted
+            print fastq_formatted
             if num > 100:
                 break
+
+        # if sequence is contaminated with adapater
+        if name in not_pristine:
+            outfile_contaminated.write(fastq_formatted + '\n')
+            continue
+
         else:
-            outfile.write(fasta_formatted + '\n')
+            outfile_pristine.write(fastq_formatted + '\n')
 
     if not test_run:
-        outfile.close()
+        outfile_contaminated.close()
+        outfile_pristine.close()
 
-def run(input_psl_file, input_file, output_file, test_run):
+def run(input_psl_file, input_file, outfile_root, test_run):
     """identifies reads not to be written, then writes everything else"""
     not_pristine = get_corrupt_seq_names(input_psl_file, test_run)
-    write_pristine(input_file, output_file, not_pristine, test_run)
+    write_pristine(input_file, outfile_root, not_pristine, test_run)
     print '\n Done!'
 
 if __name__ == "__main__":
@@ -59,7 +67,7 @@ if __name__ == "__main__":
     script_info['script_usage']=[]
     script_info['script_usage'].append(
         ("Example 1","""Test run of write pristine:""",
-        """python get_pristine_seqs.py -p <somefile.psl> -i <seqs.fasta> -o <pristine_seqs.fasta> -t"""))
+        """python get_pristine_seqs.py -p <somefile.psl> -i <seqs> -o <pristine_seqs.fasta> -t"""))
 
     script_info['help_on_no_arguments'] = True
     script_info['required_options'] = [
@@ -67,8 +75,10 @@ if __name__ == "__main__":
                     help='The input psl file from blat'),
         make_option('-i','--input_file',
                     help='The input fasta sequence file'),
-        make_option('-o','--output_file',
-                    help='The file to write pristine seqs to'),
+        make_option('-o','--outfile_root',
+                    help='The pristine and contaminated files will be written '\
+                    'to two different files, with this as this + _pristine or '\
+                    '_contaminated as the name of the file'),
         ]
 
     script_info['optional_options'] = [\
@@ -80,4 +90,4 @@ if __name__ == "__main__":
 
     parser, opts, args = parse_command_line_parameters(**script_info)
 
-    run(opts.input_psl_file, opts.input_file, opts.output_file, opts.test_run)
+    run(opts.input_psl_file, opts.input_file, opts.outfile_root, opts.test_run)

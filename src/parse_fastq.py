9866c90bd1f1c1a83a6afa58d3ed99c455811acd
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-07-30 13:55:07 +1000

    improvements to speed of parse_fastq
    [CHANGED] given how simple this format is, especially that actual
        sequences and quality data are on single lines, we just count
        line number to figure out what data is on that line.
    [CHANGED] the FastqParser now uses LightSeq as it's default
        constructor.

--- src/parse_fastq.py
+++ src/parse_fastq.py
@@ -3,56 +3,61 @@ from cogent.parse.fasta import Info
 
 from light_seq import LightSeq
 
-def MinimalFastqParser(data):
+def MinimalFastqParser(data, strict=True):
     """yields name, seq, qual from fastq file
+    
+    Arguments:
+        - strict: checks the quality and sequence labels are the same
     """
     if type(data) == str:
         data = open(data)
-
+    
+    # fastq format is very simple, defined by blocks of 4 lines
     seq = None
     qual = None
     seq_label = None
-    qual_label = None
-    num = 0
+    line_num = -1
     for line in data:
-        if line[0] == '@': # is seq block
-            seq_label = line[1:].strip()
-            qual_label = None
-        elif line[0] == '+':# is qual block
-            qual_label = line[1:].strip()
-            assert qual_label == seq_label
-        elif seq_label is not None and qual_label is None:
+        line_num += 1
+        
+        if line_num == 0:
+            if line[0] != '@': # could just be empty line at end-of-file
+                line_num = -1
+                continue
+            seq_label = line[1:].rstrip()
+        elif line_num == 1:
             seq = line.strip()
-        elif seq_label is not None and qual_label is not None:
+        elif line_num == 3:
+            line_num = -1
             qual = line.strip()
             yield seq_label, seq, qual
-            seq = None
-            qual = None
-            seq_label = None
-            qual_label = None
+        elif strict: # must be line == 2
+            qual_label = line[1:].strip()
+            assert qual_label == seq_label, 'Invalid format'
+    
+    if type(data) == file:
+        data.close()
+    
 
 
-
-def _make_seq(seq, name, qual):
-    """makes a DnaSeq object"""
-    info = Info(qual=qual)
-    seq = DNA.makeSequence(seq, name)
-    seq.Info = info
-    return name, seq
-
-def FastqParser(data, remove_ambig=True, trim_bad_bases=True,
-                make_seq=LightSeq):
+def FastqParser(data, numeric_qual=False, remove_ambig=True,
+    trim_bad_bases=True, make_seq=None, strict=True):
     """yields name, seq from fastq file
 
     Arguments:
         - data is a data series (e.g. file, list)
+        - numeric_qual: whether base quality score is returned or raw ASCII
         - remove_ambig: excludes any sequence with N
         - trim_bad_bases: trims terminal positions with B quality score
-        - make_seq: a function that takes seq, name, qual and returns data
+        - make_seq: a function that takes name, seq, qual and returns data,
+          defaults to the LightSeq class
     """
 
+    if make_seq is None:
+        make_seq = LightSeq
+
     ambig_count = 0
-    parser = MinimalFastqParser(data)
+    parser = MinimalFastqParser(data, strict=strict)
     for name, seq, qual in parser:
         if remove_ambig:
             if 'N' in seq:
@@ -63,18 +68,26 @@ def FastqParser(data, remove_ambig=True, trim_bad_bases=True,
                 ambig_count+=1
                 continue
 
-        seq_object = make_seq(Seq=seq, Name=name, Quality=qual)
-
         if trim_bad_bases:
             # we find B and trim seq and qual
             bad = qual.find('B')
             if bad == 0:
                 continue
             elif bad > 0:
-                seq_object = seq_object[:bad]
+                seq = seq[: bad]
+                qual = qual[: bad]
 
-        if len(seq_object) == 0:
+        if len(seq) == 0:
             continue
 
-        yield seq_object
+        if numeric_qual:
+            qual = [v-64 for v in map(ord, qual)]
 
+        yield make_seq(seq, name, qual)
+
+if __name__ == "__main__":
+    parser = MinimalFastqParser('../data/s_6_sequence.txt')
+    r = 0
+    for record in parser:
+        r += 1
+    print r

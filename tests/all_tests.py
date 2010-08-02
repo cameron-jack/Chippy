d92bfc146d64ed0673cbde605150be26d62efd30
anujp <anuj.pahwa@gmail.com>
2010-08-02 17:20:08 +1000

    Merged Code

---
+++ tests/all_tests.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+#
+# suite of unit tests.
+# run suite by executing this file
+#
+
+import sys, os
+os.chdir('tests')
+sys.path.append(os.path.expanduser('~cggroup/repos/PyCogent-trunk'))
+
+import doctest, cogent.util.unit_test as unittest, sys, os
+
+def my_import(name):
+    """Imports a module, possibly qualified with periods. Returns the module.
+    
+    __import__ only imports the top-level module.
+    
+    Recipe from python documentation at:
+    http://www.python.org/doc/2.4/lib/built-in-funcs.html
+    """
+    mod = __import__(name)
+    components = name.split('.')
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+def suite():
+    modules_to_test = [
+        'test_bowtie_output',
+        'test_inline_stats',
+        'test_light_seq',
+        'test_parse_fastq',
+        'test_parse_psl',
+        ]
+    
+    alltests = unittest.TestSuite()
+    
+    for module in modules_to_test:
+        test = unittest.findTestCases(my_import(module))
+        alltests.addTest(test)
+    return alltests
+
+class BoobyTrappedStream(object):
+    def __init__(self, output):
+        self.output = output
+    
+    def write(self, text):
+        self.output.write(text)
+        raise RuntimeError, "Output not allowed in tests"
+    
+
+if __name__ == '__main__':
+    if '--debug' in sys.argv:
+        s = suite()
+        s.debug()
+    else:
+        orig = sys.stdout
+        if '--output-ok' in sys.argv:
+            sys.argv.remove('--output-ok')
+        else:
+            sys.stdout = BoobyTrappedStream(orig)
+        try:
+            unittest.main(defaultTest='suite', argv=sys.argv)
+        finally:
+            sys.stdout = orig
--- tests/test_parse_psl.py
+++ tests/test_parse_psl.py
@@ -18,15 +18,19 @@ class Test(TestCase):
             'Q gap bases', 'T gap count', 'T gap bases', 'strand', 'Q name',
             'Q size', 'Q start', 'Q end', 'T name', 'T size', 'T start',
             'T end', 'block count', 'blockSizes', 'qStarts', 'tStarts']
-
+        
         parser = MinimalPslParser(fname)
         version = parser.next()
         header = parser.next()
         self.assertEqual(header, expect)
+<<<<<<< local
 
+=======
+    
+>>>>>>> other
     def test_psl_to_table(self):
         table = PslToTable(fname)
-
+    
     def test_getting_seq_coords(self):
         """get correct sequence coordinates to produce a trimmed sequence"""
         table = PslToTable(fname)
@@ -35,7 +39,7 @@ class Test(TestCase):
             query_strand = row["strand"]
             q_start = row["Q start"]
             #print query_name, q_start, query_strand
-
+    
 
 if __name__ == "__main__":
     main()

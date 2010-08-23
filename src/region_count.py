1ef1efd7eca34bc47745206702e03617d34be3ad
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-08-23 12:21:01 +1000

    region count class mods
    [CHANGED] this class will no longer be involved in producing displays
        and will only be used for generating the saved counts data files
        per chromosome for all provided coordinates.

--- src/region_count.py
+++ src/region_count.py
@@ -3,31 +3,17 @@ from numpy import zeros, uint32, uint64, save, load
 
 class RegionCounts(object):
     """records sequence read counts for a genomic region"""
-    def __init__(self, length=None, filename=None):
+    def __init__(self, length):
         super(RegionCounts, self).__init__()
         
-        assert length or filename, 'RegionCounts requires either'\
-                                         ' a length or filename'
-        if filename:
-            self.counts = load(filename)
-        else:
-            self.counts = zeros((2, length), uint32)
+        self.counts = zeros(length, uint32)
     
-    def addRead(self, start, end, strand):
+    def addRead(self, start, end):
         """add counts for range start, end"""
         
-        self.counts[strand, start: end] += 1
+        self.counts[start: end] += 1
     
-    def getCounts(self, start, end, strand=None):
-        """returns counts for the named span"""
-        segment = self.counts[:, start: end]
-        if strand is None:
-            counts = segment.sum(axis=0)
-        else:
-            counts = segment[strand]
-        return counts
-    
-    def save(self, filename):
+    def save(self, filename, ordered_gene_coords):
         """saves as in numpy binary format"""
         save(filename, self.counts)
     

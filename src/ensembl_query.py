5a5397f530ebd254789cb160c0167f9d9574e0b8
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-08-26 15:23:20 +1000

    added mouse chromosome lengths
    [NEW] ensembl querying code modified to get chromosome lengths

--- src/ensembl_query.py
+++ src/ensembl_query.py
@@ -8,16 +8,14 @@ except KeyError:
     print 'Need ENSEMBL_ACCOUNT environment variable, quitting!'
     exit(-1)
 
-def sample_ensembl(out_dir, release, write_seqs=False):
-    account = HostAccount('cg.anu.edu.au', username, password)
-    
-    mouse = Genome(Species='Mouse', Release = release, account=account)
-    
-    chroms = map(str, range(1,20)+['X', 'Y'])
+release = 58
+chroms = map(str, range(1,20)+['X', 'Y'])
+account = HostAccount('cg.anu.edu.au', username, password)
+mouse = Genome(Species='Mouse', Release = release, account=account)
+
+def sample_ensembl(out_dir, write_seqs=False):
     for chrom in chroms:
         region = mouse.getRegion(CoordName = chrom)
-        chrom_lengths += [[chrom, len(region)]]
-        
         if write_seqs:
             outfile_name = os.path.join(out_dir, 'chr_%s.fasta' % chrom)
             outfile = open(outfile_name, 'w')
@@ -25,5 +23,17 @@ def sample_ensembl(out_dir, release, write_seqs=False):
             out.close()
     
 
+def get_chrom_lengths(out_dir):
+    """writes a table of chromosome lengths"""
+    rows = []
+    for chrom in chroms:
+        region = mouse.getRegion(CoordName = chrom)
+        rows += [[chrom, len(region)]]
+    table = LoadTable(header=['chrom', 'length'], rows=rows)
+    print table
+    table.writeToFile(os.path.join(out_dir,
+                    'mouse_chrom_lengths_release_%d.txt' % release), sep='\t')
+
 if __name__ == "__main__":
-    sample_ensembl('', release=58, write_seqs=False)
+    # sample_ensembl('', release=58, write_seqs=False)
+    get_chrom_lengths("../data/")

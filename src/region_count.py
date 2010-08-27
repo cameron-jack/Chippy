293f4d8942f4f88e4d30a65e18d21632ddbcf410
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-08-27 15:10:41 +1000

    changes to region_count
    [NEW] add an arg to specify whether the mapping software counts from 0 or one
    [CHANGED] code style changes. Only class methods get mixedCamelCase. Variables
        get noun_with_underscores. See the coding style guidelines.
        http://pycogent.sourceforge.net/coding_guidelines.html
    [CHANGED] the RegionCount.save method just needs the start site and strand.
    [CHANGED] the save object needs to be defined as uint16 too (I think).
    [FIXED] the save method uses window_size to bracket the TSS, and strand to
        specify the order in which the data are saved.

--- src/region_count.py
+++ src/region_count.py
@@ -1,36 +1,48 @@
-import time
 from numpy import zeros, uint16, save, load
 
 class RegionCounts(object):
     """records sequence read counts for a genomic region"""
-    def __init__(self, length):
+    def __init__(self, length, one_based=True):
         super(RegionCounts, self).__init__()
-
         # using uint16 allows for a maximum count value of 65535 which *should*
         # be enough.
         self.counts = zeros(length, uint16)
+        
+        # if the mapping software counts from one
+        self._adjust = [0, -1][one_based]
 
     def addRead(self, start, end):
-        """add counts for range start, end"""
+        """add counts for range start, end, adjusting for whether the number
+        system starts at 1"""
+        self.counts[self._adjust + start: end] += 1
 
-        self.counts[start: end] += 1
+    def save(self, filename, ordered_coords, window_size):
+        """saves in numpy binary format
+        
+        Arguments:
+            - filename: full path to be saved to, NOTE: numpy adds the .npy
+              suffix
+            - ordered_coords: a series with [(tss, strand), ..] where
+              tss stands for transcription start site
+            - window_size: counts in a window  of tss +/- window_size
+              saved. Reverse strand counts are reversed.
+        """
 
-    def save(self, filename, ordered_gene_coords, windowSize):
-        """saves as in numpy binary format"""
+        annotated_counts = zeros((len(ordered_coords), 2 * window_size), uint16)
 
-        annotatedCounts = zeros((len(ordered_gene_coords), windowSize))
-
-        row_index = 0
-        for [chrom, tss, five, three, strand] in ordered_gene_coords:
+        for row_index, (tss, strand) in enumerate(ordered_coords):
+            stride = strand
             # positive strand
             if strand == 1:
-                annotatedCounts[row_index] = self.counts[five:three + 1]
+                start = tss - window_size
+                end = tss + window_size
             # negative strand
             else:
-                annotatedCounts[row_index] = self.counts[three:five + 1]
-            row_index += 1
-
-        print 'Saving ' + filename + '\n\n'
-        save(filename, annotatedCounts)
-
+                # start which actually be > end
+                start = tss + window_size
+                end = tss - window_size - 1
+            
+            annotated_counts[row_index] = self.counts[start: end: stride]
+        
+        save(filename, annotated_counts)
 

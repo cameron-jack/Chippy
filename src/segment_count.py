bbd3f78298cfcbaf9d226eb861a6055f16485059
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-08-27 16:15:55 +1000

    update to match changes to region_count class
    [CHANGED] segment_count.get_gene_coords function now just returns the TSS and
        strand for genes.
    [CHANGED] get_gene_coords sorts the coord table again, we want to ensure the
        order is consistent no matter what state the coord table was saved in.
    [CHANGED] region_analysis code calls to external methods now (hopefully)
        correct
    [CHANGED] simplify region_analysis calls to table object for making a dict

--- src/segment_count.py
+++ src/segment_count.py
@@ -6,25 +6,21 @@ from cogent.util.progress_display import display_wrap
 
 from region_count import RegionCounts
 
-def get_gene_coords(infile_name, chrom_name, window, just_strand=1):
-    """returns 5', TSS, 3', strand"""
+def get_gene_coords(infile_name, chrom_name):
+    """returns TSS, strand for the nominated chromosome"""
     table = LoadTable(infile_name, sep='\t')
     table = table.filtered(lambda x: x == chrom_name, columns='CoordName')
+    table = table.sorted()
     rows = []
     for row in table:
         strand = row['Strand']
         if strand == -1:
             TSS = row['End']
-            five = TSS + window
-            three = TSS - window
         else:
             TSS = row['Start']
-            five = TSS - window
-            three = TSS + window
-
-        if strand == just_strand or just_strand == 0:
-            rows += [(chrom_name, TSS, five, three, strand)]
-
+        
+        rows += [(TSS, strand)]
+    
     return rows
 
 def get_binned_counts(a, bin_size=150):
@@ -34,15 +30,14 @@ def get_binned_counts(a, bin_size=150):
 
 @display_wrap
 def run(ui):
-    """docstring for run"""
     win_size = 2000
-    gene_TSS = get_gene_coords('../data/mouse_gene_coords.txt', 1, win_size)
+    gene_tss_strand = get_gene_coords('../data/mouse_gene_coords.txt', 1)
     treatment = RegionCounts(filename='../data/s_7_pristine-chr1.npy')
     control = RegionCounts(filename='../data/s_8_pristine-chr1.npy')
     trt_binned = None
     ctl_binned = None
-    for i in ui.series(range(len(gene_TSS))):
-        chrom, tss, five, three, strand = gene_TSS[i]
+    for i in ui.series(range(len(gene_tss_strand))):
+        tss, strand = gene_tss_strand[i]
         trt_counts = treatment.getCounts(five, three, None)
         ctl_counts = control.getCounts(five, three, None)
         if ctl_binned is None:
@@ -53,14 +48,14 @@ def run(ui):
             ctl_binned += get_binned_counts(ctl_counts)
 
     # averaged by number of genes
-    trt_binned /= len(gene_TSS)
-    ctl_binned /= len(gene_TSS)
+    trt_binned /= len(gene_tss_strand)
+    ctl_binned /= len(gene_tss_strand)
 
     diff = trt_binned - ctl_binned
 
     sd = numpy.sqrt(trt_binned + ctl_binned)
     norm_diff = diff / sd
-    norm_diff /= len(gene_TSS)
+    norm_diff /= len(gene_tss_strand)
     x = numpy.arange(-win_size+75, win_size-75)
     fig = pyplot.figure(figsize=(10,5))
     minor_locator = MultipleLocator(100)

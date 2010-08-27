0024b99b465ce37138b9e75dbae68c4956b7b103
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-08-27 16:21:39 +1000

    coding tidy and style changes in region_analysis
    [CHANGED] variables are noun_with_underscores (ie all lower case)
    [CHANGED] iterating on a dict is by key, so no need to create a separate
        object
    [CHANGED] wrap lines at <= 80 characters

--- src/region_analysis.py
+++ src/region_analysis.py
@@ -4,37 +4,36 @@ from segment_count import get_gene_coords
 from make_counts import get_read_counts_bowtie
 
 # read in the chromosome lengths and create a dictionary
-chromLengths = LoadTable('../data/mouse_chrom_lengths_release_58.txt', sep='\t')
-chromLengthsDict = dict(chromLengths.getRawData(['chrom', 'length']))
-chroms = chromLengthsDict.keys()
+chrom_lengths = LoadTable('../data/mouse_chrom_lengths_release_58.txt',
+                          sep='\t')
+chrom_lengths = dict(chrom_lengths.getRawData(['chrom', 'length']))
 
 # input files
-treatmentFile = '../../../tremethick/s_7.map'
-controlFile = '../../../tremethick/s_8.map'
-geneCoordsFile = '../data/mouse_gene_coords.txt'
+treatment_file = '../../../tremethick/s_7.map'
+control_file = '../../../tremethick/s_8.map'
+gene_coords_file = '../data/mouse_gene_coords.txt'
 
 # The area from the TSS that we are interested in
 window_size = 2000
 
 print 'Control Analysis...'
-for chrom in chroms:
+for chrom in chrom_lengths:
 
     chrom_str = 'chr' + str(chrom)
     print 'Getting Counts for ' + chrom_str
-    counter = get_read_counts_bowtie(controlFile, chrom_str, chromLengthsDict[chrom])
-    mouseGeneCoords = get_gene_coords(geneCoordsFile, chrom)
+    counter = get_read_counts_bowtie(control_file, chrom_str,
+                                     chrom_lengths[chrom])
+    mouse_gene_coords = get_gene_coords(gene_coords_file, chrom)
     print 'Saving binary for ' + chrom_str
-    counter.save('s_8_counts_%s'%chrom_str, mouseGeneCoords, window_size)
+    counter.save('s_8_counts_%s'%chrom_str, mouse_gene_coords, window_size)
 
 print 'Treatment Analysis...'
-for chrom in chroms:
+for chrom in chrom_lengths:
     chrom_str = 'chr' + str(chrom)
     print 'Getting Counts for ' + chrom_str
-    counter = get_read_counts_bowtie(treatmentFile, chrom_str, chromLengthsDict[chrom])
-    mouseGeneCoords = get_gene_coords(geneCoordsFile, chrom)
+    counter = get_read_counts_bowtie(treatment_file, chrom_str,
+                                     chrom_lengths[chrom])
+    mouse_gene_coords = get_gene_coords(gene_coords_file, chrom)
     print 'Saving binary for ' + chrom_str
-    counter.save('s_7_counts_%s'%chrom_str, mouseGeneCoords, window_size)
-
-
-
+    counter.save('s_7_counts_%s'%chrom_str, mouse_gene_coords, window_size)
 

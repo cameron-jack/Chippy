951483ec9dd27a3c3346c5fd2ad77cdd7d7cf6ef
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-09-06 10:32:42 +1000

    segment_count code using gene indices to subsample counts.
    [CHANGED] added to todo on how this file needs to be changed

--- src/segment_count.py
+++ src/segment_count.py
@@ -5,6 +5,7 @@ from cogent import LoadTable
 from cogent.util.progress_display import display_wrap
 
 from region_count import RegionCounts
+from get_gene_data import get_gene_indices
 
 def get_gene_coords(infile_name, chrom_name):
     """returns TSS, strand for the nominated chromosome"""
@@ -29,44 +30,57 @@ def get_binned_counts(a, bin_size=150):
     return numpy.array(result)
 
 @display_wrap
-def run(ui):
+def run(rdumpfilename, title, ui):
     win_size = 2000
-    gene_tss_strand = get_gene_coords('../data/mouse_gene_coords.txt', 1)
-    treatment = RegionCounts(filename='../data/s_7_pristine-chr1.npy')
-    control = RegionCounts(filename='../data/s_8_pristine-chr1.npy')
+    genes = get_gene_indices(rdumpfilename)
+    num_genes = len(genes)
+    treatment_template = '../../RegionAnalysis/s_7-window_5000-chr%s.npy'
+    control_template = '../../RegionAnalysis/s_8-window_5000-chr%s.npy'
+    treatments = {}
+    controls = {}
     trt_binned = None
     ctl_binned = None
-    for i in ui.series(range(len(gene_tss_strand))):
-        tss, strand = gene_tss_strand[i]
-        trt_counts = treatment.getCounts(five, three, None)
-        ctl_counts = control.getCounts(five, three, None)
-        if ctl_binned is None:
-            trt_binned = get_binned_counts(trt_counts)
-            ctl_binned = get_binned_counts(ctl_counts)
+    for gene in genes:
+        if gene.chrom in treatments:
+            treatment = treatments[gene.chrom]
+            control = controls[gene.chrom]
         else:
-            trt_binned += get_binned_counts(trt_counts)
-            ctl_binned += get_binned_counts(ctl_counts)
-
-    # averaged by number of genes
-    trt_binned /= len(gene_tss_strand)
-    ctl_binned /= len(gene_tss_strand)
-
+            treatment = numpy.load(treatment_template % gene.chrom)
+            control = numpy.load(control_template % gene.chrom)
+            treatments[gene.chrom] = treatment
+            controls[gene.chrom] = control
+            if trt_binned is None:
+                trt_binned = numpy.zeros(control.shape[1], float)
+                ctl_binned = numpy.zeros(control.shape[1], float)
+        ctl_binned += control[gene.index]
+        trt_binned += treatment[gene.index]
+    
+    trt_binned /= num_genes
+    ctl_binned /= num_genes
+    
     diff = trt_binned - ctl_binned
-
+    
     sd = numpy.sqrt(trt_binned + ctl_binned)
     norm_diff = diff / sd
-    norm_diff /= len(gene_tss_strand)
-    x = numpy.arange(-win_size+75, win_size-75)
+    norm_diff /= num_genes
+    norm_diff = get_binned_counts(norm_diff)
+    x = numpy.arange(-(control.shape[1]/2)+75, (control.shape[1]/2)-75)
     fig = pyplot.figure(figsize=(10,5))
     minor_locator = MultipleLocator(100)
     pyplot.plot(x, norm_diff)
     pyplot.xlabel('Position relative to TSS')
     pyplot.ylabel('Normalised difference (averaged by gene)')
-    pyplot.title('Mouse Chrom 1 Genes + strand')
+    pyplot.title("%s: N=%s" % (title, len(genes)))
     pyplot.gca().xaxis.set_minor_locator(minor_locator)
+    pyplot.setp(pyplot.gca(), ylim=(-0.5,2.5))
     pyplot.grid(True)
-    pyplot.savefig('mouse_chrom1_plus_strand.pdf')
+    pyplot.savefig('%s.pdf' % rdumpfilename)
     print '\n\nDone!'
 
 if __name__ == "__main__":
-    run()
+    rdump_filename1 = '../g2-vs-g1/paa.G2.gt.G1.sig.sort.2FC.txt'
+    title1 = 'G2 > G1'
+    rdump_filename2 = '../g2-vs-g1/paa.G2.lt.G1.sig.sort.2FC.txt'
+    title2 = 'G2 < G1'
+    
+    run(rdump_filename1, title1)

160d3616060af3fe8d6c98c02cbdbf9365c00910
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-09-18 09:07:50 +1000

    code refactor to improve flexibility of plotting
    [NEW] added a curve smoothing module to simplify the plotting of smoothed
        counts data
    [CHANGED] the GeneIndex class and factory function moved to new module,
        gene_data
    [CHANGED] old get_gene_data functionality now implemented in
        rdump_to_stable_ids
    
    The idea is we use rdump_to_stable_ids on the gene data from Rohan and generate
    a list of Ensembl Stable IDs. These are then used by the segment_count script,
    which imports the GetGeneIndexes factory function. This will allow us to check
    the results for arbitrary lists of genes, defined outside the R-process.

--- src/rdump_to_stable_ids.py
+++ src/rdump_to_stable_ids.py
@@ -39,46 +39,6 @@ def dump_gene_ids(rdump_filename, outfile_name):
     outfile.write('\n'.join(gene_ids)+'\n')
     outfile.close()
 
-class GeneIndex(object):
-    """basic object to store a chromosome and an index"""
-    def __init__(self, stable_id, chrom, index):
-        super(GeneIndex, self).__init__()
-        self.chrom = chrom
-        self.index = index
-        self.stable_id = stable_id
-    
-    def __str__(self):
-        return '%s(chrom=%s, index=%s)' % (self.stable_id, self.chrom, self.index)
-    
-    def __repr__(self):
-        return '%s(%s, %s)' % (self.stable_id, self.chrom, self.index)
-
-def GetGeneIndexes():
-    """returns func to get indexes for groups of genes"""
-    gene_indices = LoadTable('../data/mouse_gene_coords.txt', sep='\t')
-    
-    names_indices = []
-    for chrom_name in range(1,20)+['X', 'Y']:
-        chrom = gene_indices.filtered(lambda x: x == chrom_name, columns='CoordName')
-        names = chrom.getRawData('StableId')
-        for i in range(len(names)):
-            gene = GeneIndex(names[i], chrom_name, i)
-            names_indices += [(names[i], gene)]
-    
-    names_indices = dict(names_indices)
-    def call(gene_id):
-        return names_indices[gene_id]
-    
-    return call
-
-def get_gene_indices(rdump_filename):
-    gene_ids = get_gene_ids(rdump_filename)
-    get_gene_index = GetGeneIndexes()
-    genes = [get_gene_index(gene_id) for gene_id in gene_ids]
-    data = sorted([(gene.chrom, gene) for gene in genes])
-    return [g for c,g in data]
-
-
 if __name__ == "__main__":
     from cogent.util.misc import parse_command_line_parameters
     from optparse import make_option

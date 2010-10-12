c17d56a7aa78cc02aa224eee3800689b06941e32
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-10-12 17:24:56 +1100

    code to allow dumping selected genes to tab-delimited file

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -27,6 +27,7 @@ def my_import(name):
 def suite():
     modules_to_test = [
         'test_bowtie_output',
+        'test_export_counts',
         'test_inline_stats',
         'test_light_seq',
         'test_parse_fastq',

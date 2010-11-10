87f4f64ce2988f6aa8a553906b0dc07ed853b3dd
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-11-10 23:00:33 +1100

    added function to util to get unique entries, so we don't recount genes
    [NEW] discovered that the expression lists can have multiple entries
        for a gene. This could in principle lead to genes being counted
        more than once.

--- src/util.py
+++ src/util.py
@@ -28,3 +28,24 @@ def get_centred_coords(length, window_size):
     start = (length//2)- window_size
     end = start + 2 * window_size
     return start, end
+
+def unique_records(data, column=None):
+    """returns unique records in original order"""
+    try: # it's a Table
+        stable_ids = data.getDistinctValues(column)
+    except AttributeError:
+        stable_ids = set(data)
+    
+    def is_unique(x, stable_ids=stable_ids):
+        if x in stable_ids:
+            stable_ids.remove(x)
+            return True
+        return False
+    
+    try: # it's a Table
+        result = data.filtered(is_unique, columns=column)
+    except AttributeError:
+        result = filter(is_unique, data)
+    
+    return result
+

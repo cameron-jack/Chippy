3d6d7652f18d0128e5d339fdffcb70d223946e7a
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2010-11-14 15:51:57 +1100

    moved line number counting code to util and renamed

--- src/make_counts.py
+++ src/make_counts.py
@@ -18,31 +18,6 @@ def format_long(n):
 
     return ','.join(r)
 
-# following needs to be moved to client code
-def bufcount(filename):
-    # from http://stackoverflow.com/questions/845058/how-to-get-line-count-cheaply-in-python
-    f = open(filename)
-    lines = 0
-    buf_size = 1024 * 1024
-    read_f = f.read # loop optimization
-
-    buf = read_f(buf_size)
-    while buf:
-        lines += buf.count('\n')
-        buf = read_f(buf_size)
-
-    return lines
-
-def get_file_length(filename):
-    """output number of lines in read file"""
-
-    start = time.time()
-    print 'Reading %s' % filename
-    num_lines = bufcount(filename)
-    end = time.time()
-    print format_long(num_lines) + ' lines read in ' + \
-            ('%fs' % ((end-start)/60.))
-
 def get_read_counts_sam(infile_name, chrom_name):
     """Generate the read counts using the SAM format infile"""
 
@@ -100,5 +75,4 @@ if __name__ == "__main__":
     # get_read_counts_sam('../data/s_8_pristine.map', 'chr1')
 
     # manually supplying the length of the chromosome for testing purposes.
-    get_file_length('../data/bowtie_output.map')
     get_read_counts_bowtie('../data/bowtie_output.map', 'chr1', 197195432)

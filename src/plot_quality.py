270a5e38748c511a09398b3d7c2bc3b5fbc7cafc
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2011-01-26 15:23:12 +1100

    updated for compatability with PyCogent.app script forms

--- src/plot_quality.py
+++ src/plot_quality.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+from optparse import make_option
 import numpy as np
 import matplotlib.pyplot as plt
+from cogent.util.misc import parse_command_line_parameters
 
 from inline_stats import RunningStats
 from parse_fastq import FastqParser, MinimalFastqParser
@@ -90,37 +92,37 @@ def plot_quality(input_file, upper_quantile, lower_quantile, sample_freq,
     make_display(stats, output_file, upper_quantile, lower_quantile,
                 sample_freq, num)
 
-if __name__ == "__main__":
-    from cogent.util.misc import parse_command_line_parameters
-    from optparse import make_option
+# 
+script_info = {}
+descr = "Get Quality Information from a fastq file and plot the quality "\
+        "scores across all bases."
+script_info['brief_description']= descr
+script_info['script_description'] = descr
+script_info['version'] = '0.1.alpha'
+script_info['script_usage']=[]
+script_info['script_usage'].append(
+    ("Example 1","""General Usage:""",
+    """python plot_quality.py -i <seqs.fastq> -o <qual_plot.png>"""))
 
-    script_info = {}
-    descr = "Get Quality Information from a fastq file and plot the quality "\
-            "scores across all bases."
-    script_info['brief_description']= descr
-    script_info['script_description'] = descr
-    script_info['version'] = '0.1.alpha'
-    script_info['script_usage']=[]
-    script_info['script_usage'].append(
-        ("Example 1","""General Usage:""",
-        """python plot_quality.py -i <seqs.fastq> -o <qual_plot.png>"""))
+script_info['help_on_no_arguments'] = True
+script_info['required_options'] = [
+    make_option('-i','--input_file', help='The input fastq sequence file')]
+script_info['optional_options'] = [
+    make_option('-o','--output_file',
+                help='The file to store the quality plot to. If not '\
+                'specified, plot is stored to quality.png'),
+    make_option('-u', '--upper_quantile', type='float', default=0.95,
+        help='upper quantile [default: %default]'),
+    make_option('-l', '--lower_quantile', type='float', default=0.05,
+        help='lower quantile [default: %default]'),
+    make_option('-s', '--sample_freq', type='int', default=1,
+        help='sample frequency [default: %default]'),
+    make_option('-L', '--limit', type='int', default=0,
+        help='number of sequences to limit to [default: %default]'),
+        ]
 
-    script_info['help_on_no_arguments'] = True
-    script_info['required_options'] = [
-        make_option('-i','--input_file', help='The input fastq sequence file')]
-    script_info['optional_options'] = [
-        make_option('-o','--output_file',
-                    help='The file to store the quality plot to. If not '\
-                    'specified, plot is stored to quality.png'),
-        make_option('-u', '--upper_quantile', type='float', default=0.95,
-            help='upper quantile [default: %default]'),
-        make_option('-l', '--lower_quantile', type='float', default=0.05,
-            help='lower quantile [default: %default]'),
-        make_option('-s', '--sample_freq', type='int', default=1,
-            help='sample frequency [default: %default]'),
-        make_option('-L', '--limit', type='int', default=0,
-            help='number of sequences to limit to [default: %default]'),
-            ]
+
+def main():
     parser, opts, args = parse_command_line_parameters(**script_info)
     plot_quality(opts.input_file,
         opts.upper_quantile,
@@ -129,3 +131,5 @@ if __name__ == "__main__":
         opts.output_file,
         opts.limit or None)
 
+if __name__ == "__main__":
+    main()

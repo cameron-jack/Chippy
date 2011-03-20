74ba8857b5bb78a7759b75343e775ab7dce2010e
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2011-03-20 15:23:28 +1100

    mods to fastq_to_fasta
    [CHANGED] make compatible with pycogent.app script form API
    [CHANGED] now takes a RunRecord object for recording run statistics

--- src/fastq_to_fasta.py
+++ src/fastq_to_fasta.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 
 from os.path import basename, dirname, join
+
 from cogent.parse.fastq import MinimalFastqParser
+from cogent.util.misc import parse_command_line_parameters
+from optparse import make_option
+
 from util import create_path
 from parse_fastq import FastqParser
 
-def run(input_file, output_file, minimum_length, rewrite, test_run):
+def run(input_file, save_dir, output_file, minimum_length, rewrite, run_record, test_run):
     if not test_run:
-        outfile_fasta = open(output_file, 'w')
-        outfile_directory = dirname(output_file)
-        create_path(outfile_directory)
+        assert '/' not in output_file, \
+                'Do not put directory path in output_file'
+        
+        create_path(save_dir)
+        outfile_fasta = open(os.path.join(save_dir, output_file), 'w')
         if rewrite:
             outfile_fastq_fn = basename(output_file).split('.')[0] + '_trimmed.fastq'
-            outfile_fastq = open(join(outfile_directory, outfile_fastq_fn), 'w')
+            outfile_fastq = open(join(save_dir, outfile_fastq_fn), 'w')
     
     i = 0
     num_too_small = 0
@@ -40,44 +46,57 @@ def run(input_file, output_file, minimum_length, rewrite, test_run):
         outfile_fasta.close()
         if rewrite:
             outfile_fastq.close()
+    
+    run_record.append(['fastq_to_fasta', 'stdout', 'Sequences read', i])
+    run_record.append(['fastq_to_fasta', 'stdout', 'Sequences discarded as too small', num_too_small])
+    return run_record
 
-    print 'Success!!\n%d Sequences were read' % i
-    print '%d Sequences were discarded as too small' % num_too_small
+# 
+script_info = {}
+descr = "Convert fastq file to a fasta file after quality checks on fastq file and "\
+        "only choosing sequences that meet specified criteria"
+script_info['brief_description']= descr
+script_info['script_description'] = descr
+script_info['version'] = '0.1.alpha'
+script_info['script_usage']=[]
+script_info['script_usage'].append(
+    ("Example 1","""General Usage:""",
+    """python fastq_to_fasta.py -i <seqs.fastq> -o <seqs.fasta>"""))
+
+script_info['help_on_no_arguments'] = True
+script_info['required_options'] = [
+    make_option('-i','--input_file',
+                help='The input fastq sequence file'),
+    make_option('-o','--output_file',
+                help='The file to write fasta seqs to'),
+    make_option('-S', '--save_dir',
+                help='path to save all files')
+    ]
+
+script_info['optional_options'] = [\
+    make_option('-t','--test_run', action='store_true',
+                dest='test_run', default = False,
+                help='Dry run without writing any data'
+                +'[default: %default]'),
+    make_option('-l', '--minimum_length', type='int',
+                default=35,
+                help='minimum length of sequences to write [default: %default]'),
+    make_option('-q', '--rewrite_fastq', action='store_true',
+                help='Rewrite input fastq file after removing discarded sequences')
+                ]
+
+def main():
+    run_record = []
+    parser, opts, args = parse_command_line_parameters(**script_info)
+    run_record = run(opts.input_file, opts.output_file, opts.minimum_length,
+            opts.rewrite_fastq, run_record, opts.test_run)
+    return run_record
 
 if __name__ == "__main__":
-    from cogent.util.misc import parse_command_line_parameters
-    from optparse import make_option
+    from cogent import LoadTable
+    run_record = main()
+    table = LoadTable(header=['command', 'pipe', 'message', 'value'],
+        rows=run_record)
+    print table
 
-    script_info = {}
-    descr = "Convert fastq file to a fasta file after quality checks on fastq file and "\
-            "only choosing sequences that meet specified criteria"
-    script_info['brief_description']= descr
-    script_info['script_description'] = descr
-    script_info['version'] = '0.1.alpha'
-    script_info['script_usage']=[]
-    script_info['script_usage'].append(
-        ("Example 1","""General Usage:""",
-        """python fastq_to_fasta.py -i <seqs.fastq> -o <seqs.fasta>"""))
-
-    script_info['help_on_no_arguments'] = True
-    script_info['required_options'] = [
-        make_option('-i','--input_file',
-                    help='The input fastq sequence file'),
-        make_option('-o','--output_file',
-                    help='The file to write fasta seqs to')
-        ]
-
-    script_info['optional_options'] = [\
-        make_option('-t','--test_run', action='store_true',
-                    dest='test_run', default = False,
-                    help='Dry run without writing any data'
-                    +'[default: %default]'),
-        make_option('-l', '--minimum_length', type='int',
-                    default=35,
-                    help='minimum length of sequences to write [default: %default]'),
-        make_option('-q', '--rewrite_fastq', action='store_true',
-                    help='Rewrite input fastq file after removing discarded sequences')
-                    ]
-
-    parser, opts, args = parse_command_line_parameters(**script_info)
-    run(opts.input_file, opts.output_file, opts.minimum_length, opts.rewrite_fastq, opts.test_run)
+    

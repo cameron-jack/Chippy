feec5687dab019f49284bd3bc132fc6b74465530
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2011-03-24 14:49:13 +1100

    generalised functions withim chippy.ref* to any Ensembl genome

--- chippy/ref/get_gene_transcript_map.py
+++ chippy/ref/get_gene_transcript_map.py
@@ -1,25 +1,33 @@
 from cogent import LoadTable
-from cogent.db.ensembl import *
+from cogent.db.ensembl import Genome, HostAccount
 
-account = HostAccount('cg.anu.edu.au', 'compgen', 'compgen')
-mouse = Genome('mouse', Release=58,account=account)
-
-def run():
-    transcript_gene = []
-    bio_types = mouse.getDistinct('BioType')
+def gene_transcript_mappings(species, release, account=None, debug=False):
+    """returns {gene: [transcripts,,], ..} and {transcript: gene, ..}"""
+    transcript_gene = {}
+    gene_transcript = {}
+    genome = Genome(species, Release=release, account=account)
+    bio_types = genome.getDistinct('BioType')
+    n = 0
     for bio_type in bio_types:
-        genes = mouse.getGenesMatching(BioType=bio_type)
-        for index, gene in enumerate(genes):
-            print index, gene.Symbol
+        genes = genome.getGenesMatching(BioType=bio_type)
+        for gene in genes:
             gene_stable_id = gene.StableId
-            gene_symbol = gene.Symbol
-            for transcript in gene.Transcripts:
-                transcript_stable_id = transcript.StableId
-                transcript_gene += [[bio_type, transcript_stable_id, gene_stable_id, gene_symbol]]
-
-
-    table = LoadTable(header=['BioType', 'TranscriptId', 'GeneId', 'GeneSymbol'], rows=transcript_gene)
-    table.writeToFile('../data/ensembl_transcript_gene_ids.txt', sep='\t')
+            transcript_ids = [t.StableId for t in gene.Transcripts]
+            gene_transcript[gene_stable_id] = transcript_ids
+            transcript_gene.update(dict([(gene_stable_id, t)
+                        for t in transcript_ids]))
+            n += 1
+            if n % 100 == 0 and debug:
+                return gene_transcript, transcript_gene
+    
+    return gene_transcript, transcript_gene
 
 if __name__ == "__main__":
-    run()
+    account = HostAccount('cg.anu.edu.au', 'gavin', 'gavin')
+    species = 'mouse'
+    release = 58
+    g, t = gene_transcript_mappings(species, release, account, True)
+    
+    print g
+    print
+    print t
--- chippy/ref/get_genome.py
+++ chippy/ref/get_genome.py
@@ -1,45 +1,29 @@
-import os
-from cogent import LoadTable
-from cogent.db.ensembl import HostAccount, Species, Genome
+from cogent.db.ensembl import Genome, HostAccount
 
-try:
-    ensembl_account = os.environ['ENSEMBL_ACCOUNT'].split()
-except KeyError:
-    print 'Need ENSEMBL_ACCOUNT environment variable, quitting!'
-    exit(-1)
-
-try:
-    host, username, password = ensembl_account
-except ValueError:
-    username, password = ensembl_account
-    host = 'cg.anu.edu.au'
-
-release = 58
-chroms = map(str, range(1,20)+['X', 'Y'])
-account = HostAccount(host, username, password)
-mouse = Genome(Species='Mouse', Release = release, account=account)
-
-def sample_ensembl(out_dir, write_seqs=False):
+def get_chrom_seqs(species, release, chroms, account=None):
+    """yields sequence objects for the indicated chromosomes from Ensembl"""
+    genome = Genome(species, Release=release, account=account)
     for chrom in chroms:
-        region = mouse.getRegion(CoordName = chrom)
-        if write_seqs:
-            outfile_name = os.path.join(out_dir, 'chr_%s.fasta' % chrom)
-            outfile = open(outfile_name, 'w')
-            outfile.write(region.Seq.toFasta()+'\n')
-            outfile.close()
+        region = genome.getRegion(CoordName = chrom)
+        seq = region.Seq
+        name = 'chr_%s.fasta' % chrom
+        seq.Name = name
+        yield seq
 
-
-def get_chrom_lengths(out_dir):
-    """writes a table of chromosome lengths"""
-    rows = []
-    for chrom in chroms:
-        region = mouse.getRegion(CoordName = chrom)
-        rows += [[chrom, len(region)]]
-    table = LoadTable(header=['chrom', 'length'], rows=rows)
-    print table
-    table.writeToFile(os.path.join(out_dir,
-                    'mouse_chrom_lengths_release_%d.txt' % release), sep='\t')
 
 if __name__ == "__main__":
-    # sample_ensembl('', release=58, write_seqs=False)
-    get_chrom_lengths("../data/")
+    host = 'cg.anu.edu.au'
+    username = password = 'gavin'
+    release = 58
+    chroms = map(str, range(1,20)+['X', 'Y'])
+    account = HostAccount(host, username, password)
+    print account
+    species = 'mouse'
+    for chrom in get_chrom_seqs(species, release, chroms, account):
+        print chrom.Name, len(chrom)
+        fasta = chrom[:10].toFasta()
+        print fasta
+        # outfile_name = os.path.join(out_dir, )
+        # outfile = open(outfile_name, 'w')
+        # outfile.write(fasta+'\n')
+        # outfile.close()

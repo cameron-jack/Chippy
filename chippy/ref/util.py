e141e3500fdf82befe483d8dad82189132afef0d
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2011-03-30 11:10:02 +1100

    change reference genome chromosome names to tuple from list
    [CHANGED] should be immutable so can't be screwed with by client code

--- chippy/ref/util.py
+++ chippy/ref/util.py
@@ -8,6 +8,6 @@ __status__ = "alpha"
 __version__ = '0.1'
 
 # we ignore mitochondria
-mouse_chroms = map(str, range(1,20)+['X', 'Y'])
-human_chroms = map(str, range(1,23)+['X', 'Y'])
+mouse_chroms = tuple(map(str, range(1,20)+['X', 'Y']))
+human_chroms = tuple(map(str, range(1,23)+['X', 'Y']))
 chroms = dict(mouse=mouse_chroms, human=human_chroms)

2ccfe47b3d71c95e4ca2c3133c2198c4fa163ed2
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2011-04-04 13:54:41 +1000

    mods to db_query
    [CHANGED] get_ranked_expression can now use the reference file
    [NEW] db_query now can has a diff_expression_study that compares the genes
        in an expression study with those identified in a expression study source
        file, identifying the differences and their reasons.

--- chippy/express/db_query.py
+++ chippy/express/db_query.py
@@ -2,12 +2,14 @@ from sqlalchemy import and_
 from sqlalchemy.orm import contains_eager
 from sqlalchemy.orm.exc import NoResultFound
 
+from cogent import LoadTable
 from cogent.util.progress_display import display_wrap
 from cogent.util.misc import flatten
 
 from chippy.express.db_schema import Gene, Transcript, Exon, \
             ExternalGene, Expression, ExpressionDiff, ReferenceFile, Sample, \
             Session, Base, make_session
+from chippy.express.db_populate import get_transcript_gene_mapping, single_gene
 from chippy.ref.util import chroms
 
 __author__ = "Gavin Huttley"
@@ -18,6 +20,15 @@ __maintainer__ = "Gavin Huttley"
 __email__ = "Gavin.Huttley@anu.edu.au"
 __status__ = "alpha"
 __version__ = '0.1'
+
+def _one(query):
+    """returns result if found, False otherwise"""
+    try:
+        result = query.one()
+    except NoResultFound:
+        result = False
+    
+    return result
 
 def _get_sample(session, sample_name):
     try:
@@ -31,16 +42,41 @@ def get_samples(session):
     samples = session.query(Sample).all()
     return samples
 
-def get_ranked_expression(session, ensembl_release, sample_name, test=False):
+def get_ranked_expression(session, ensembl_release, sample_name, data_path=None, test=False):
     """returns all ranked genes from a sample"""
     sample = _get_sample(session, sample_name)
+    if data_path is not None:
+        reffile = _one(session.query(ReferenceFile).filter_by(name=data_path))
+        if reffile is None:
+            raise RuntimeError('No reference file record for that path')
+    
     if sample is None:
         raise RuntimeError('Unknown sample name: %s' % sample_name)
-    
-    expressed = session.query(Expression).join(Gene).filter(
+    if test:
+        if data_path is None:
+            total = session.query(Expression).filter(
+            Expression.sample_id==sample.sample_id).count()
+        else:
+            total = session.query(Expression).filter(
+            and_(Expression.sample_id==sample.sample_id,
+                Expression.reference_file_id==reffile.reference_file_id)
+                ).count()
+        
+        print 'Num expression records for sample in db', total
+        
+    if data_path is None:
+        query = session.query(Expression).join(Gene).filter(
             and_(Gene.ensembl_release==ensembl_release,
                  Expression.sample_id==sample.sample_id)).order_by(
-                 Expression.rank).options(contains_eager('gene')).all()
+                 Expression.rank).options(contains_eager('gene'))
+    else:
+        query = session.query(Expression).join(Gene).filter(
+            and_(Gene.ensembl_release==ensembl_release,
+                 Expression.sample_id==sample.sample_id,
+                 Expression.reference_file_id==reffile.reference_file_id)
+                 ).order_by(Expression.rank).options(contains_eager('gene'))
+    
+    expressed = query.all()
     return expressed
 
 def get_ranked_genes_per_chrom(session, ensembl_release, sample_name, chrom, test=False):
@@ -92,3 +128,57 @@ def get_total_gene_counts(session, ensembl_release, sample_name, test=False):
             and_(Gene.ensembl_release==ensembl_release,
                  Expression.sample_id==sample.sample_id)).count()
     return total
+
+
+@display_wrap
+def diff_expression_study(session, sample_name, data_path, table,
+            ensembl_release='58', ensembl_id_label='ENSEMBL', test=False, ui=None):
+    """returns list of genes and why they're not present in an existing
+    expression db"""
+    # query db for every gene linked to this expression study
+    records = get_ranked_expression(session, ensembl_release, sample_name,
+            data_path=data_path, test=test)
+    # 
+    transcript_to_gene = get_transcript_gene_mapping(session, ensembl_release)
+    failures = []
+    kept_gene_ids_probesets = []
+    for record in ui.series(table, noun='Adding expression diffs'):
+        transcript_ids = record[ensembl_id_label]
+        gene_id = single_gene(transcript_to_gene, transcript_ids)
+        probeset_id = record['probeset_id']
+        if gene_id is None:
+            failures.append(['probeset', probeset_id, 
+                            'probeset id maps to multiple genes'])
+            continue
+        kept_gene_ids_probesets.append([gene_id, probeset_id])
+    
+    in_db_gene_ids = set([e.gene_id for e in records])
+    in_table_kept_gene_ids = set([g for g,p in kept_gene_ids_probesets])
+    in_db_not_table = in_db_gene_ids - in_table_kept_gene_ids
+    if in_db_not_table:
+        failures.extend([('dberror', gid,
+                    'population error! Contact developers')
+                for gid in in_db_not_table])
+    
+    in_table_not_db = in_table_kept_gene_ids - in_db_gene_ids
+    if in_table_not_db:
+        failures.extend([('gene', gid, 'gene mapped to multiple probesets')
+                                                for gid in in_table_not_db])
+    
+    run_record = LoadTable(header=['Type', 'Id', 'Explanation'], rows=failures,
+    title='Difference between db for'\
+        +' %(sample)s and %(reffile)s' % dict(reffile=data_path,
+                                               sample=sample_name))
+    
+    failed_probsets = run_record.count("Type == 'probeset'")
+    dup_gene_transcripts = run_record.count("Type == 'gene'")
+    dberror = run_record.count("Type == 'dberror'")
+    summary = LoadTable(header=['Label', 'Number'],
+        rows=[['Num genes in db', len(records)],
+              ['Probsets that do not map to a unique gene', failed_probsets],
+              ['Genes with multiple transcripts', dup_gene_transcripts],
+              ['Database population errors', dberror]],
+              title='Summary of difference')
+    
+    return summary, run_record
+

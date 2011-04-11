299d0e9ce395b3dbeabdc2a8a23626aeb910e529
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2011-04-11 15:37:40 +1000

    removed dead classes in read_count.py
    [CHANGED] totally dropped old way of doing things with the RegionCount
        class. Replaced by the WholeChrom class.

--- tests/test_read_count.py
+++ tests/test_read_count.py
@@ -6,86 +6,7 @@ import numpy
 from cogent import LoadTable
 from cogent.util.unit_test import TestCase, main
 from cogent.util.misc import remove_files
-from chippy.core.read_count import RegularRegionCounts, IrregularRegionCounts, WholeChrom
-
-class RegionCountTests(TestCase):
-    def test_handle_one_zero_based_systems(self):
-        """should correctly handle zero or one based counting system"""
-        count_start = 1
-        count_end = 10
-        counter = RegularRegionCounts(100, one_based=True)
-        counter.addRead(count_start, count_end)
-        self.assertEqual(counter.counts[0:10], numpy.ones(10, numpy.uint16))
-        
-        counter = RegularRegionCounts(100, one_based=False)
-        counter.addRead(count_start, count_end)
-        expect = numpy.ones(10, numpy.uint16)
-        expect[0] = 0
-        self.assertEqual(counter.counts[0:10], expect)
-    
-    def test_subregion_counts(self):
-        """should return correct counts for subregions"""
-        counter = RegularRegionCounts(100, one_based=False)
-        
-        counter.addRead(20, 30)
-        counter.addRead(75, 85)
-        
-        tss_strand = [(20, 1), (84, -1)]
-        
-        subregions = counter._get_subregion_counts(tss_strand, 10)
-        self.assertEqual(subregions.shape, (2, 20))
-        col_sums = subregions.sum(axis=0)
-        expect = numpy.zeros(20, numpy.uint16)
-        expect[10:] = 2
-        self.assertEqual(col_sums, expect)
-    
-    def test_save_counts(self):
-        """correctly saves to a file"""
-        file_name = 'saved_counts'
-        counter = RegularRegionCounts(100, one_based=False)
-        counter.addRead(20, 30)
-        counter.addRead(75, 85)
-        tss_strand = [(20, 1), (84, -1)]
-        expect = counter._get_subregion_counts(tss_strand, 10)
-        fname = file_name+'.npz'
-        counter.save(file_name, tss_strand, 10)
-        got = numpy.load(fname)
-        got = [got[i] for i in got.files] # just expect one array
-        self.assertEqual(got, expect)
-        remove_files([fname])
-    
-class IrregularRegionCountTests(TestCase):
-    def test_get_irregular_regions(self):
-        """correctly get regions counts with different lengths"""
-        counter = IrregularRegionCounts(100, one_based=False)
-        coords = [(20, 30), (70, 90)]
-        for coord in coords:
-            counter.addRead(*coord)
-        
-        subregions = counter._get_subregion_counts([(20, 30, 1),
-                        (70, 90, -1)])
-        for i in range(2):
-            coord = coords[i]
-            subregion = subregions[i]
-            self.assertEqual(subregion.sum(), coord[1]-coord[0])
-    
-    def test_saving_counts(self):
-        """correctly save an irregular region count object"""
-        counter = IrregularRegionCounts(100, one_based=False)
-        coords = [(20, 30, 1), (70, 90, -1)]
-        for coord in coords:
-            counter.addRead(*coord[:2])
-        
-        expect = counter._get_subregion_counts(coords)
-        
-        file_name = 'saved_irregular_counts'
-        
-        fname = '%s.npz' % file_name
-        counter.save(file_name, coords)
-        got = numpy.load(fname)
-        for i in range(len(got.files)):
-            self.assertEqual(got[str(i)], expect[i])
-        remove_files([fname])
+from chippy.core.read_count import WholeChrom
 
 # sample counts for below
 # Read num / pos    0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25
@@ -94,6 +15,7 @@ class IrregularRegionCountTests(TestCase):
 #     (- strand)  2 .   .   .   2   2   2   2   2   2   2   2   2   2   .   .   .   .   .   .   .   .   .   .   .   .   .
 #                 3 .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   10  10  10  10  10  10  10  10  10  10  .
 #         counts    0   0   2   5   5   5   5   5   5   5   5   5   2   0   0   10  10  10  10  10  10  10  10  10  10  0
+
 
 class MinimalRegionCountTests(TestCase):
     def setUp(self):

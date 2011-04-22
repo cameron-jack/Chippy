f67b009d8fd6847afae97bfb7463b0a2442fed55
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2011-04-22 21:10:49 +1000

    changes to parsing R dump style files, 100% test coverage of chippy.parse.r_dump
    [CHANGED] removed dead code from parsing Rohan's old style dumps with quoted
        strings etc ..
    [NEW] allow_probeset_many_gene argument to SimpleRdumpToTable. When True,
        any probeset that maps to multiple genes is excluded. This now the
        default behaviour.
    [NEW] remove_probesets function takes a single row, a series of probesets to
        be excluded and removes them and associated expression scores
    [CHANGED] SimpleRdumpToTable now doesn't require the order of labels to always
        be the same.

--- chippy/parse/r_dump.py
+++ chippy/parse/r_dump.py
@@ -11,98 +11,13 @@ __email__ = "Gavin.Huttley@anu.edu.au"
 __status__ = "alpha"
 __version__ = '0.1'
 
-def sans_quotes(val):
-    """returns string without quotes"""
-    return val.replace("'", "").replace('"', '')
-
-def standardised_strand(val):
-    """converts character representation into -1/1 to be consistent with the
-    mapping software format"""
-    val = sans_quotes(val)
-    if val == '+':
-        val = 1
-    elif val == '-':
-        val = -1
-    else:
-        return None
-    
-    return val
-
-def split_ensembl_ids(val):
-    """returns the ensembl IDs split into elements"""
-    val = sans_quotes(val)
-    ids = val.split('|')
-    return ids
-
-def chrom_num(val):
-    """returns just the chromosome ID"""
-    val = sans_quotes(val)
-    return val.replace('chr', '')
-
-def cast_num(val):
-    """int or float"""
-    try:
-        val = int(val)
-    except ValueError:
-        val = float(val)
-    return val
-
-def get_reader(infile):
-    """generates a reader for the infile"""
-    for num, line in enumerate(infile):
-        if num == 0:
-            line = map(sans_quotes, line.split('\t'))
-            strand_index = line.index('strand')
-            id_index = line.index('ENSEMBL')
-        else:
-            break
-    
-    conversions = []
-    for index, field in enumerate(line.strip().split('\t')):
-        if index == strand_index:
-            conversions += [(index, standardised_strand)]
-        elif index == id_index:
-            conversions += [(index, split_ensembl_ids)]
-        else:
-            field = eval(field)
-            type_ = type(field)
-            if type_ == str:
-                type_ = sans_quotes
-            elif type_ in (int, float):
-                type_ = cast_num
-        
-            conversions += [(index, type_)]
-    
-    infile.seek(0)
-    converter = ConvertFields(conversions)
-    reader = SeparatorFormatParser(with_title=True, converter=converter,
-            sep='\t')
-    return reader(infile)
-
-def RDumpParser(data):
-    """docstring for RDumpParser"""
-    if type(data) == str:
-        data = open(data)
-    reader = get_reader(data)
-    header = map(sans_quotes, reader.next())
-    yield header
-    for line in reader:
-        yield line
-
-def RDumpToTable(data):
-    """returns a cogent table object"""
-    parser = RDumpParser(data)
-    header = parser.next()
-    rows = [l for l in parser]
-    return LoadTable(header=header, rows=rows, space=2)
-
 def convert(to_float=False, strict=False):
     """converts a | separated string into a tuple of floats or ints"""
     type_ = [int, float][to_float]
     def call(value):
         result = value.strip().split('|')
         try:
-            result = tuple(map(type_, result))
+            result = map(type_, result)
         except ValueError:
             # can occur as older Affy arrays have probeset IDs that mix
             # int/str
@@ -110,11 +25,34 @@ def convert(to_float=False, strict=False):
                 raise ValueError
             pass
         
-        return result
+        return tuple(result)
     
     return call
 
-def SimpleRdumpToTable(path, sep='\t', stable_id_label='', probeset_label='', exp_label='', validate=True):
+def remove_probesets(row, probesets, probeset_index, exp_index):
+    """returns a new row without the problem probesets or their expression"""
+    row = list(row)
+    current_probesets = list(row[probeset_index])
+    current_exp = list(row[exp_index])
+    del_indices = map(current_probesets.index, probesets)
+    new_probesets = []
+    new_exp = []
+    for i in range(len(current_probesets)):
+        if i in del_indices:
+            continue
+        
+        new_probesets.append(current_probesets[i])
+        new_exp.append(current_exp[i])
+    
+    if len(new_probesets) == 0:
+        return None
+    
+    row[probeset_index] = tuple(new_probesets)
+    row[exp_index] = tuple(new_exp)
+    return row
+
+def SimpleRdumpToTable(path, sep='\t', stable_id_label='', probeset_label='',
+        exp_label='', allow_probeset_many_gene=False, validate=True):
     """returns a cogent table object
     
     Handles case where probset id's and expressions scores are separated by
@@ -125,16 +63,33 @@ def SimpleRdumpToTable(path, sep='\t', stable_id_label='', probeset_label='', ex
         - probeset_label: name of column containing probesets
         - exp_label: name of column containing expression scores
         - stable_id_label: name of column containing Ensembl stable IDs
+        - allow_probeset_many_gene: whether one probeset can map to multiple
+          genes
         - validate: checks that -- stable IDs are unique in the file, that
           for each row the number of probesets equals the number of expression
           scores. Raises a RuntimeException if failure occurs for any
           of these checks.
     """
+    # forces reading in as string
+    converter = ConvertFields([])
+    reader = SeparatorFormatParser(with_title=True, converter=converter,
+            sep='\t')
     
-    converter = ConvertFields([(1, convert(to_float=False)),
-                               (2, convert(to_float=True))])
-    reader = SeparatorFormatParser(converter=converter, sep='\t')
     table = LoadTable(path, reader=reader)
+    
+    # convert '|' delimited probeset and expression fields to tuples of
+    # (ints / string) and int respectively
+    convert_probeset = convert(to_float=False)
+    convert_exp = convert(to_float=True)
+    probeset_index = table.Header.index(probeset_label)
+    exp_index = table.Header.index(exp_label)
+    rows = table.getRawData()
+    for row in rows:
+        row[probeset_index] = convert_probeset(row[probeset_index])
+        row[exp_index] = convert_exp(row[exp_index])
+    
+    table = LoadTable(header=table.Header, rows=rows)
+    
     if validate:
         assert probeset_label and exp_label and stable_id_label,\
             'Must provide all required column labels to validate'
@@ -144,8 +99,43 @@ def SimpleRdumpToTable(path, sep='\t', stable_id_label='', probeset_label='', ex
         
         for row in table:
             if len(row[probeset_label]) != len(row[exp_label]):
-                raise RuntimeError(
-                        'Mismatched number of probesets and exp scores')
+                gene_id = row[stable_id_label]
+                raise RuntimeError('Mismatched number of probesets and '\
+                        'exp scores for %s' % gene_id)
+            
+        
+    
+    # look for cases where a probeset maps to multiple genes
+    if not allow_probeset_many_gene:
+        probeset_gene = {}
+        gene_problem_probesets = {}
+        for row in table:
+            stable_id = row[stable_id_label]
+            for probeset in row[probeset_label]:
+                try: # bad probeset, record it and affected genes
+                    probeset_gene[probeset].update([stable_id])
+                    for gene_id in probeset_gene[probeset]:
+                        gene_problem_probesets[gene_id] = \
+                                    gene_problem_probesets.get(gene_id, set())
+                        gene_problem_probesets[gene_id].update([probeset])
+                
+                except KeyError:
+                    probeset_gene[probeset] = set([stable_id])
+        
+        # what indices for the essential columns
+        id_index = table.Header.index(stable_id_label)
+        probeset_index = table.Header.index(probeset_label)
+        exp_index = table.Header.index(exp_label)
+        rows = []
+        for row in table.getRawData():
+            stable_id = row[id_index]
+            if stable_id in gene_problem_probesets:
+                row = remove_probesets(row, gene_problem_probesets[stable_id],
+                            probeset_index, exp_index)
+            if row is not None:
+                rows.append(row)
+        
+        table = LoadTable(header=table.Header, rows=rows)
     
     return table
 

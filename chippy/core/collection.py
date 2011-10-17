[FIXED] Standard deviation plots now work correctly (normalized standard devs) [FIXED] Grid is now properly turned off rather than just white-on-white

chippy/core/collection.py
@@ -21,6 +21,11 @@ def column_sum(data):
     assert len(data.shape) == 2
     return data.sum(axis=0)
 
+def stdev(data):
+    """returns the column sums for the purpose of creating stdevs"""
+    assert len(data.shape) == 2
+    return data.sum(axis=0)
+
 def _make_mean(axis):
     def call(data):
         return data.mean(axis=axis)
@@ -211,8 +216,10 @@ class RegionCollection(_GenericCollection):
     def transformed(self, rank_func=rank_mean, counts_func=column_mean):
         """transforms all counts and ranks"""
         c = counts_func(self.counts)
-        if counts_func == column_stdev:
-            c = c - numpy.mean(c, axis=0)
+        if counts_func == stdev:
+            mean = c.mean()
+            stdev_ = c.std(ddof=1)
+            c = (c - mean)/stdev_
         if self.ranks is not None:
             r = rank_func(self.ranks)
         else:
@@ -260,9 +267,9 @@ class RegionCollection(_GenericCollection):
             # row
             data = self.counts.max(axis=1)
             mean = self.counts.mean()
-            stdev = self.counts.std(ddof=1)
+            stdev_ = self.counts.std(ddof=1)
             data -= mean
-            data /= stdev
+            data /= stdev_
             indices = data < k
             data = self.counts[indices]
             if self.labels is not None:
@@ -352,8 +359,10 @@ class RegionCollection(_GenericCollection):
                     counts_func=column_mean):
         for counts, ranks, labels in self.itergroups(group_size):
             c = counts_func(counts)
-            if counts_func == column_stdev:
-                c = c - numpy.mean(c, axis=0)
+            if counts_func == stdev:
+                mean = c.mean()
+                stdev_ = c.std(ddof=1)
+                c = (c - mean)/stdev_
             r = rank_func(ranks)
             yield c, r, labels
     
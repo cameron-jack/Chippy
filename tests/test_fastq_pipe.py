11c6fa8f3848525b9100b5d426170f99d24170c1
Depression Account <Depression Account@localhost>
2011-11-01 16:33:45 +1100

    imporve error message in running pipeline

--- tests/test_fastq_pipe.py
+++ tests/test_fastq_pipe.py
@@ -4,6 +4,7 @@ import sys, os
 sys.path.append('..')
 
 from cogent.util.unit_test import TestCase, main
+from cogent.util.misc import remove_files
 
 from chippy.prep.command_line import run_command
 
@@ -13,7 +14,7 @@ class FastqPipe(TestCase):
 
         # Check that we're not going to nuke something we shouldn't
         if os.path.exists('test_pipe_dir'):
-            raise RuntimeError('Cannot run test, "test_pipe_dir" already exists')
+            remove_files(['test_pipe_dir'], error_on_missing=False)
 
         # Run pipeline with known fastq file
         call_string = '../chippy/prep/fastq_to_mapped.py -i data/known_sequence.fq '\
@@ -23,12 +24,12 @@ class FastqPipe(TestCase):
 
         returncode, stdout, stderr = run_command(call_string)
         if returncode != 0:
-            raise RuntimeError('command line app exited with an error')
+            raise RuntimeError('command line app exited with an error: %s' % stderr)
         # Generate md5 checksums of result files
         call_string = 'md5sum test_pipe_dir *.gz > test_pipe_dir/results.md5'
         returncode, stdout, stderr = run_command(call_string)
         if returncode != 0:
-            raise RuntimeError('command line app exited with an error')
+            raise RuntimeError('command line app exited with an error: %s' % stderr)
         
         # Compare values. Note: this could fail with user altered parameters
         resulting_md5 = open ('test_pipe_dir/results.md5')

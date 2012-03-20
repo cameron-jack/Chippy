29c378c126ac84f74d5cfd6025b294fcb636022f
Cameron Jack <cameron.jack@anu.edu.au>
2012-03-20 15:38:26 +1100

    [NEW] Illumina Casava v1.8+ support added

--- chippy/prep/fastq_to_mapped_bam.py
+++ chippy/prep/fastq_to_mapped_bam.py
@@ -5,7 +5,7 @@
 - produces trimmed sequences without adpaters
 - maps the clean sequences using bowtie
 """
-import sys, re, os, shutil
+import sys, re, os, shutil, zipfile
 sys.path.extend(['..', '../src'])
 
 import numpy
@@ -15,7 +15,7 @@ from optparse import make_option
 
 from chippy.prep import reduce, pristine_seqs, command_line, fastq_to_fasta, \
      pristine_paired
-from chippy.prep.mapped_files import MappedFiles
+from chippy.prep.mapped_files import MappedFiles, MappedSangerFiles
 from chippy.util.run_record import RunRecord
 from chippy.util.util import create_path
 
@@ -71,10 +71,7 @@ script_info['optional_options'] = [\
     make_option('-I', '--Illumina_version', default=1.7,
                 help='Illumina pipeline version number'
                 +' [default: %default]'),
-    make_option('--no_qual', action='store_true', default=False,
-                help='Do not use any quality control'
-                +' [default: %default]'),
-    make_option('-p', '--pval_cutoff', type='float', default=0.001,
+    make_option('-p', '--pval_cutoff', type='float', default=0.05,
         help='Minimum p-value for mapping quality of reads '\
              '[default: %default]')
 ]
@@ -83,132 +80,169 @@ def main():
     option_parser, opts, args =\
         parse_command_line_parameters(**script_info)
 
-    # make the assorted filenames
+    rr = RunRecord() # create the run audit tracker
 
-    rr = RunRecord() # the run audit tracker
+    ### We support different two very different pipelines depending on Illumina version
 
-    mapped_files_1 = MappedFiles(opts.input_file_1, opts.save_dir, opts.work_dir)
-    filenames_1 = dict(fastq=mapped_files_1.adapterless_trimmed_fn,
+    # Make the assorted filenames required
+
+    ## v1.3 - v1.7 pipeline:
+    # Zipped or unzipped, remove 5' adapters with fastx_clipper on each end
+    # Remove poor quality bases and reads with fastx_quality_trimmer on each end
+    # Balance unmatched read pairs
+    # Run bwa aln on each end
+    # Run bwa sampe and save as either BAM or reduced
+
+    if 1.3 >= opts.Illumina_version <= 1.7:
+        mapped_files_1 = MappedFiles(opts.input_file_1, opts.save_dir, opts.work_dir)
+        filenames_1 = dict(fastq=mapped_files_1.adapterless_trimmed_fn,
                        pristine=mapped_files_1.pristine_fn, sai=mapped_files_1.sai_fn,
                        bam=mapped_files_1.bam_fn, sam=mapped_files_1.sam_fn)
-    mapped_files_2 = MappedFiles(opts.input_file_2, opts.save_dir, opts.work_dir)
-    filenames_2 = dict(fastq=mapped_files_2.adapterless_trimmed_fn,
+        mapped_files_2 = MappedFiles(opts.input_file_2, opts.save_dir, opts.work_dir)
+        filenames_2 = dict(fastq=mapped_files_2.adapterless_trimmed_fn,
                        pristine=mapped_files_2.pristine_fn, sai=mapped_files_2.sai_fn,
                        bam=mapped_files_2.bam_fn)
 
-    if os.path.exists(filenames_1['bam']):
-        rr.addInfo('fastq_to_mapped_bam', 'already exists, exiting', filenames_1['bam'])
+    ## v1.8+
+    # Check if gzipped, if yes then unzip - raw
+    # Run Perl DynamicTrim.pl on each end (outputs as filename.ext.trimmed) - trimmed
+    # Run Sickle on both ends (we name the out files plus the singles file) - balanced
+    # Run bwa aln on each end - sai
+    # Run bwa sampe and save as either BAM or reduced
+
+    elif opts.Illumina_version >= 1.8: # Illumina pipeline 1.8 or higher
+        mapped_files_1 = MappedSangerFiles(opts.input_file_1, opts.save_dir, opts.work_dir)
+        filenames_1 = dict(unzipped=mapped_files_1.unzipped_fq_fn, trimmed=mapped_files_1.trimmed_fn,
+                pristine=mapped_files_1.pristine_fn, singles=mapped_files_1.singles_fn,
+                sai=mapped_files_1.sai_fn, bam=mapped_files_1.bam_fn,
+                sam=mapped_files_1.sam_fn)
+
+        mapped_files_2 = MappedSangerFiles(opts.input_file_2, opts.save_dir, opts.work_dir)
+        filenames_2 = dict(unzipped=mapped_files_2.unzipped_fq_fn, trimmed=mapped_files_2.trimmed_fn,
+            pristine=mapped_files_2.pristine_fn, sai=mapped_files_2.sai_fn)
+
+    else: # unsupported Illumina pipeline version
+        rr.addError('fastq_to_mapped_bam', 'Invalid Illumina pipeline given. v1.3+ supported',
+            opts.Illumina_version)
         rr.display()
         sys.exit(0)
 
-    if not opts.no_qual:
-        if 1.3 >= opts.Illumina_version <= 1.7:
-
-            ## do input file 1
-            if opts.begin <= 1 and opts.end >= 1:
-                rr = command_line.run_fastx_clip_and_trim(opts.adapters, opts.input_file_1,
-                                                  filenames_1['fastq'], rr, opts.num_threads, opts.test_run)
-
-            ## do input file 2
-            if opts.begin <= 2 and opts.end >= 2:
-                rr = command_line.run_fastx_clip_and_trim(opts.adapters, opts.input_file_2,
-                                                  filenames_2['fastq'], rr, opts.num_threads, opts.test_run)
-
-            ## produce the paired pristine seq files with matched reads
-            if opts.begin <= 3 and opts.end >= 3:
-                # rr = command_line.run_pristine_paired(
-                rr = command_line.run_pristine_seesaw(opts.input_file_1,
-                                              filenames_1['fastq'], filenames_2['fastq'],
-                                              filenames_1['pristine'], filenames_2['pristine'],
-                                              rr, opts.num_threads, opts.test_run)
-
-        elif opts.Illumina_version >= 1.8: # Illumina pipeline 1.8 or higher
-            # Don't worry about 5' adapter clipping
-            # Though we might want to think about 3' adapters in future
-
-            # Quality check with Sickle (UC Davis), discard single-ends residual
-            quality_window = 20;
-            min_length = 19;
-            rr = command_line.run_sickle_pe(opts.input_file_1, opts.input_file_2,
-                            filenames_1['pristine'], filenames_2['pristine'],
-                            quality_window, min_length, rr, opts.test_run)
-
-        else:
-            rr.addError('fastq_to_mapped_bam', 'Invalid Illumina pipeline given. v1.3+ supported',
-                        opts.Illumina_version)
+    # Make sure we don't overwrite any existing output BAM file
+    if not opts.reduce and os.path.exists(filenames_1['bam']):
+            rr.addInfo('fastq_to_mapped_bam', 'already exists, exiting', filenames_1['bam'])
             rr.display()
             sys.exit(0)
 
-        ## now align each individually
-        if opts.begin <= 4 and opts.end >= 4:
-            rr = command_line.run_bwa_aln(opts.index,
+    # Quality control pipeline - depending on input data version
+    if 1.3 >= opts.Illumina_version <= 1.7:
+
+        # do input file 1
+        if opts.begin <= 1 and opts.end >= 1:
+            rr = command_line.run_fastx_clip_and_trim(opts.adapters, opts.input_file_1,
+                        filenames_1['fastq'], rr, opts.num_threads, opts.test_run)
+
+        # do input file 2
+        if opts.begin <= 2 and opts.end >= 2:
+            rr = command_line.run_fastx_clip_and_trim(opts.adapters, opts.input_file_2,
+                        filenames_2['fastq'], rr, opts.num_threads, opts.test_run)
+
+        # produce the paired pristine seq files with matched reads
+        if opts.begin <= 3 and opts.end >= 3:
+            # rr = command_line.run_pristine_paired(
+            rr = command_line.run_pristine_seesaw(opts.input_file_1,
+                        filenames_1['fastq'], filenames_2['fastq'],
+                        filenames_1['pristine'], filenames_2['pristine'],
+                        rr, opts.num_threads, opts.test_run)
+
+    elif opts.Illumina_version >= 1.8: # Illumina pipeline 1.8 or higher
+        # Check if zipped, and if yes, then unzip to work directory
+            # otherwise copy so DynamicTrim can work in-place
+        if opts.begin <= 1 and opts.end >= 1:
+            if zipfile.is_zipfile(opts.input_file_1):
+                with zipfile(opts.input_file_1, 'r') as zip1:
+                    zipfile.extractall(opts.work_dir)
+                    rr.addInfo('fastq_to_mapped_bam', 'file unzipped to work dir', opts.input_file_1)
+            else:
+                shutil.copy(opts.input_file_1, filenames_1['unzipped'])
+                rr.addInfo('fastq_to_mapped_bam', 'file copied to work dir', opts.input_file_1)
+
+            if zipfile.is_zipfile(opts.input_file_2):
+                with zipfile(opts.input_file_2, 'r') as zip2:
+                    zipfile.extractall(opts.work_dir)
+                    rr.addInfo('fastq_to_mapped_bam', 'file unzipped to work dir', opts.input_file_2)
+            else:
+                shutil.copy(opts.input_file_2, filenames_2['unzipped'])
+                rr.addInfo('fastq_to_mapped_bam', 'file copied to work dir', opts.input_file_2)
+
+        if opts.begin <= 2 and opts.end >= 2:
+            # Don't worry about 5' adapter clipping, though we might want to think about 3' adapters in future
+
+            # Remove low quality bases with DynamicTrim (SolexaQA)
+            rr = command_line.run_dynamic_trim(filenames_1['unzipped'], opts.save_dir, opts.pval_cutoff, rr, opts.test_run)
+            rr = command_line.run_dynamic_trim(filenames_2['unzipped'], opts.save_dir, opts.pval_cutoff, rr, opts.test_run)
+
+        if opts.begin <= 3 and opts.end >= 3:
+            # Balance inputs files with Sickle (UC Davis), discard single-ends residual
+            min_length = 19;
+            rr = command_line.run_sickle_pe(filenames_1['trimmed'], filenames_2['trimmed'],
+                        filenames_1['pristine'], filenames_2['pristine'],
+                        filenames_1['singles'], min_length, rr, opts.test_run)
+
+    else:
+        rr.addError('fastq_to_mapped_bam', 'Invalid Illumina pipeline given. v1.3+ supported',
+                    opts.Illumina_version)
+        rr.display()
+        sys.exit(0)
+
+    ## now align each individually
+    if opts.begin <= 4 and opts.end >= 4:
+        rr = command_line.run_bwa_aln(opts.index,
                                       filenames_1['pristine'],
                                       filenames_1['sai'],
                                       opts.Illumina_version,
                                       rr, opts.num_threads, opts.test_run)
 
-        if opts.begin <= 5 and opts.end >= 5:
-            rr = command_line.run_bwa_aln(opts.index,
+    if opts.begin <= 5 and opts.end >= 5:
+        rr = command_line.run_bwa_aln(opts.index,
                                       filenames_2['pristine'],
                                       filenames_2['sai'],
                                       opts.Illumina_version,
                                       rr, opts.num_threads, opts.test_run)
 
-        ## direct bwa_sampe to final sorted bam, bypassing sam
-        if opts.begin <= 6 and opts.end >= 6:
-            # rr = command_line.bwa_sampe_to_sorted_bam(opts.index,
-            rr = command_line.bwa_sampe_to_local_bam_sort_index_move(opts.index,
-                                      filenames_1['sai'], filenames_2['sai'],
-                                      filenames_1['pristine'], filenames_2['pristine'],
-                                      filenames_1['bam'],
-                                      rr, opts.sample_name, opts.mem_usage, opts.test_run)
+    if opts.begin <= 6 and opts.end >= 6:
+        if opts.reduce: # output to uncompressed SAM
+            rr = command_line.run_bwa_sampe_raw(opts.index,
+                    filenames_1['sai'], filenames_2['sai'],
+                    opts.input_file_1, opts.input_file_2,
+                    filenames_1['sam'], rr, opts.test_run)
 
-    else:
-        ## read direct from fastq
-        if opts.begin <= 4 and opts.end >= 4:
-            rr = command_line.run_bwa_aln(opts.index,
-                                      opts.input_file_1,
-                                      filenames_1['sai'],
-                                      opts.Illumina_version,
-                                      rr, opts.num_threads, opts.test_run)
+        else: # direct bwa_sampe to final sorted BAM, bypassing SAM
+            rr = command_line.bwa_sampe_to_sorted_bam(opts.index,
+                    filenames_1['sai'], filenames_2['sai'],
+                    opts.input_file_1, opts.input_file_2,
+                    filenames_1['bam'],
+                    rr, opts.sample_name, opts.mem_usage, opts.test_run)
 
-        if opts.begin <= 5 and opts.end >= 5:
-            rr = command_line.run_bwa_aln(opts.index,
-                                      opts.input_file_2,
-                                      filenames_2['sai'],
-                                      opts.Illumina_version,
-                                      rr, opts.num_threads, opts.test_run)
-
-
-        if opts.begin <= 6 and opts.end >= 6:
-            if opts.reduce: # output to uncompressed SAM
-                rr = command_line.run_bwa_sampe_raw(opts.index,
-                                      filenames_1['sai'], filenames_2['sai'],
-                                      opts.input_file_1, opts.input_file_2,
-                                      filenames_1['sam'], rr, opts.test_run)
-
-            else: # direct bwa_sampe to final sorted BAM, bypassing SAM
-                rr = command_line.bwa_sampe_to_sorted_bam(opts.index,
-                                      filenames_1['sai'], filenames_2['sai'],
-                                      opts.input_file_1, opts.input_file_2,
-                                      filenames_1['bam'],
-                                      rr, opts.sample_name, opts.mem_usage, opts.test_run)
-
-    if opts.reduce:
-        rr = reduce.run(infile_name=filenames_1['sam'],
+    ## index bam or output to reduced format
+    if opts.begin <= 7 and opts.end >= 7:
+        if opts.reduce:
+            rr = reduce.run(infile_name=filenames_1['sam'],
                 outdir=opts.save_dir, chroms='Do All', pval_cutoff=opts.pval_cutoff,
                 limit=numpy.inf, run_record=rr, dry_run=opts.test_run)
+
+        else:
+            rr = command_line.index_bam(filenames_1['bam'], rr, opts.test_run)
+
 
     if opts.delete:
         os.remove(filenames_1['sai'])
         os.remove(filenames_2['sai'])
         os.remove(filenames_1['pristine'])
         os.remove(filenames_2['pristine'])
-        os.remove(filenames_1['sam']) # Keep BAM
+        if opts.reduce:
+            os.remove(filenames_1['sam'])
 
-        ## index final bam
-#    if opts.begin <= 7 and opts.end >= 7:
-#        rr = command_line.index_bam(filenames_1['bam'], rr, opts.test_run)
+
 
     ## output audit
     rr.display()

0e21b74e5f9e386fedbcc92b1c726e916017c1ec
Cameron Jack <cameron.jack@anu.edu.au>
2012-12-31 13:26:01 +1100

    [NEW] Chroms DB table to remove hard coded species chroms [CHANGED] Chrom name handling during db populate [CHANGED] RunRecord usage improved throughout

--- chippy/ref/util.py
+++ chippy/ref/util.py
@@ -1,14 +1,46 @@
-__author__ = "Gavin Huttley"
-__copyright__ = "Copyright 2011, Anuj Pahwa, Gavin Huttley"
-__credits__ = ["Gavin Huttley"]
+import sys
+sys.path.extend(['..'])
+from chippy.util.run_record import RunRecord
+
+__author__ = "Gavin Huttley, Cameron Jack"
+__copyright__ = "Copyright 2012, Gavin Huttley, Cameron Jack, Anuj Pahwa"
+__credits__ = ["Gavin Huttley, Cameron Jack"]
 __license__ = "GPL"
-__maintainer__ = "Gavin Huttley"
-__email__ = "Gavin.Huttley@anu.edu.au"
-__status__ = "alpha"
+__maintainer__ = "Cameron Jack"
+__email__ = "cameron.jack@anu.edu.au"
+__status__ = "pre-release"
 __version__ = '0.1'
 
 mouse_chroms = tuple(map(str, range(1,20)+['X', 'Y', 'MT']))
 human_chroms = tuple(map(str, range(1,23)+['X', 'Y', 'MT']))
 cerevisiae_chroms = tuple(['I','II','III','IV','V','VI','VII','VIII',
         'IX','X','XI','XII','XIII','XIV','XV','XVI','MT'])
-chroms = dict(mouse=mouse_chroms, human=human_chroms)
+chroms = dict(mouse=mouse_chroms, human=human_chroms,
+        yeast=cerevisiae_chroms)
+
+# implement as a singleton, so we can use this anywhere
+# this implementation may need to be changed if converted to Python3
+class Chromosomes:
+    __single = None
+    def __init__(self, species, chroms):
+        # chroms should be a list
+        if Chromosomes.__single:
+            raise Chromosomes.__single
+        Chromosomes.__single = self
+        self.species = species
+        self.chroms = chroms
+
+def chromHandle(species='mouse', chroms=mouse_chroms, rr=RunRecord()):
+    """chrom_handle provides the safe way of creating an instance of
+    Chrom and returning THE existing instance of Chromosomes. """
+    try:
+        chromsInstance = Chromosomes(species, chroms)
+        rr.addInfo('chromHandle', 'Chromosomes instance created', True)
+        rr.addInfo('chromHandle', 'Chromosomes instance species', species)
+        rr.addInfo('chromHandle', 'Chromosomes instance chromosomes', chroms)
+    except Chromosomes, c:
+        chromsInstance = c
+        if species or chroms:
+            rr.addWarning('chromHandle', 'provided species', species)
+            rr.addWarning('chromHandle', 'provided chromosomes', chroms)
+    return chromsInstance, rr

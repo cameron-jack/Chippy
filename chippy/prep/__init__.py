698d7479f91327a2f5fa0036935183b70a4e56b1
Cameron Jack <cameron.jack@anu.edu.au>
2013-01-04 00:56:55 +1100

    [CHANGED] ChIP-Seq processing pipeline code removed ~3500 lines.

--- chippy/prep/__init__.py
+++
@@ -1,8 +0,0 @@
-__author__ = "Gavin Huttley"
-__copyright__ = "Copyright 2011, Anuj Pahwa, Gavin Huttley"
-__credits__ = ["Gavin Huttley"]
-__license__ = "GPL"
-__maintainer__ = "Gavin Huttley"
-__email__ = "Gavin.Huttley@anu.edu.au"
-__status__ = "alpha"
-__version__ = '0.1'

34d881d8a4638323be1193c6b8f59d510eaac7e0
Cameron Jack <cameron.jack@anu.edu.au>
2013-01-06 22:21:22 +1100

    Ownership notices updated

--- chippy/draw/__init__.py
+++ chippy/draw/__init__.py
@@ -1,8 +1,8 @@
-__author__ = "Gavin Huttley"
-__copyright__ = "Copyright 2011, Anuj Pahwa, Gavin Huttley"
-__credits__ = ["Gavin Huttley"]
+__author__ = "Gavin Huttley, Cameron Jack"
+__copyright__ = "Copyright 2012, Gavin Huttley, Cameron Jack, Anuj Pahwa"
+__credits__ = ["Gavin Huttley", "Cameron Jack"]
 __license__ = "GPL"
-__maintainer__ = "Gavin Huttley"
-__email__ = "Gavin.Huttley@anu.edu.au"
-__status__ = "alpha"
+__maintainer__ = "Cameron Jack"
+__email__ = "cameron.jack@anu.edu.au"
+__status__ = "Pre-release"
 __version__ = '0.1'
---
+++ chippy/util/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Gavin Huttley, Cameron Jack"
+__copyright__ = "Copyright 2012, Gavin Huttley, Cameron Jack, Anuj Pahwa"
+__credits__ = ["Gavin Huttley, Cameron Jack"]
+__license__ = "GPL"
+__maintainer__ = "Cameron Jack"
+__email__ = "cameron.jack@anu.edu.au"
+__status__ = "Pre-release"
+__version__ = '0.1'
